5f27c0ced9f2a3c993ed47ac1f3e718c3544ef05
luisburigo <guga.burigo29@outlook.com>
2023-08-29 12:17:32 -0300

    chore: eslint sort import and prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
 import { Button } from '@chakra-ui/react';
+import { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
+import { ChakraBaseProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { ChakraBaseProvider } from '@chakra-ui/react';
-import { defaultTheme } from './themes';
 import { BrowserRouter } from 'react-router-dom';
+
+import App from '@/App.tsx';
+import { defaultTheme } from '@/themes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
