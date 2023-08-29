1acff40540d810ce2440e0f83c888fe390256eb8
luisburigo <guga.burigo29@outlook.com>
2023-08-28 22:47:53 -0300

    chore: add react-router-dom axios and xstate

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { ChakraBaseProvider } from '@chakra-ui/react';
 import { defaultTheme } from './themes';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraBaseProvider theme={defaultTheme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraBaseProvider>
   </React.StrictMode>,
 );
