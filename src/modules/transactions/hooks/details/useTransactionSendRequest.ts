497a65323701dd4c3a88e0d18ce69af238096201
luisburigo <guga.burigo29@outlook.com>
2023-09-11 22:21:07 -0300

    feat(transactions): connect send transactions in bsafe sdk

--- src/modules/transactions/hooks/details/useTransactionSendRequest.ts
+++ src/modules/transactions/hooks/details/useTransactionSendRequest.ts
@@ -1,15 +1,7 @@
-import {
-  hexlify,
-  InputType,
-  InputValue,
-  Predicate,
-  Provider,
-  TransactionRequest,
-  transactionRequestify,
-  TransactionResponse,
-} from 'fuels';
+import { InputType, Predicate } from 'fuels';
 import { useMutation, UseMutationOptions } from 'react-query';
 
+import { TransactionHelpers } from '@/modules';
 import {
   GetTransactionResponse,
   TransactionService,
@@ -20,66 +12,31 @@ export interface SendTransferParams {
   predicate: Predicate<InputType[]>;
 }
 
-const sendTransaction = async (provider: Provider, tx: TransactionRequest) => {
-  const encodedTransaction = hexlify(tx.toTransactionBytes());
-  const {
-    submit: { id: transactionId },
-  } = await provider.operations.submit({ encodedTransaction });
-
-  const response = new TransactionResponse(transactionId, provider);
-  return response;
-};
-
-const instanceOldTransaction = async (
-  transaction: GetTransactionResponse,
-  predicate: Predicate<InputValue[]>,
-) => {
-  const txData = JSON.parse(transaction.txData);
-  const _transaction = transactionRequestify(txData);
-
-  _transaction.witnesses = transaction.witnesses;
-
-  const provider: Provider = predicate.provider;
-
-  const response = await sendTransaction(provider, _transaction);
-
-  const result = await response.waitForResult();
-
-  //todo: on success result, access endpoint close to update status and sendTime
-  console.log(result.receipts);
-  console.log(result.status);
-
-  if (result.status?.type == 'success') {
-    // todo: call function set done transaction
-    const gasUsed = result.gasUsed.format();
-
-    await TransactionService.done(transaction?._id, {
-      gasUsed,
-      transactionResult: JSON.stringify(result),
-    });
-  }
-};
-
 const sendTransfer = async ({ transaction, predicate }: SendTransferParams) => {
-  if (!transaction?.predicateID) {
-    alert(
-      JSON.stringify({
-        type: 'error',
-        title: 'Failed send transaction',
-      }),
-    );
-    return;
+  const transactionInstance = await TransactionHelpers.instanceTransaction({
+    predicate,
+    assets: transaction.assets,
+    witnesses: transaction.witnesses,
+  });
+
+  const result = await transactionInstance.sendTransaction();
+
+  if (result.status !== 'success') {
+    throw new Error('Error to send transaction.');
   }
 
-  const predicateInstance = predicate;
-  if (!predicateInstance) {
-    return;
-  }
-  return instanceOldTransaction(transaction, predicateInstance);
+  return TransactionService.done(transaction?._id, {
+    gasUsed: result.gasUsed,
+    transactionResult: JSON.stringify(result),
+  });
 };
 
 const useTransactionSendRequest = (
-  options?: UseMutationOptions<void, unknown, SendTransferParams>,
+  options?: UseMutationOptions<
+    GetTransactionResponse,
+    unknown,
+    SendTransferParams
+  >,
 ) => {
   return useMutation('transaction/send', sendTransfer, options);
 };
