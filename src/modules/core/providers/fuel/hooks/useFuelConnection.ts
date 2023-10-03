1ca18e873c1bc4cc6c4f76c5ba5641f3aca48148
luisburigo <guga.burigo29@outlook.com>
2023-10-03 00:09:27 -0300

    fix(singin): change account

--- src/modules/core/providers/fuel/hooks/useFuelConnection.ts
+++ src/modules/core/providers/fuel/hooks/useFuelConnection.ts
@@ -28,6 +28,17 @@ const useFuelConnection = (params?: UserFuelConnectionProps) => {
   }, [fuel]);
 
   useEffect(() => {
+    async function changeAccount() {
+      if (!account) return;
+
+      const provider = await fuel.getProvider();
+      params?.onChangeAccount(account, provider.url);
+    }
+
+    changeAccount();
+  }, [account]);
+
+  useEffect(() => {
     async function handleConnection() {
       try {
         setIsValidAccount(false);
@@ -35,13 +46,11 @@ const useFuelConnection = (params?: UserFuelConnectionProps) => {
         const isConnected = await fuel.isConnected();
         const authAccounts = await fuel.accounts();
         const account = await fuel.currentAccount();
-        const provider = await fuel.getProvider();
 
         if (authAccounts.includes(account)) {
           setAccount(account);
           setIsConnected(isConnected);
           setIsValidAccount(true);
-          params?.onChangeAccount(account, provider.url);
         }
       } catch (e) {
         setIsValidAccount(false);
