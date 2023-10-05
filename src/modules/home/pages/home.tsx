38f5012670554e53f663770a339e0b98958afd48
Pedro Barão <phbarao@gmail.com>
2023-10-04 22:03:35 -0300

    feat: wip - add recently used vaults section

--- src/modules/home/pages/home.tsx
+++ src/modules/home/pages/home.tsx
@@ -1,22 +1,34 @@
 import {
   Box,
   Button,
+  Heading,
   HStack,
   Icon,
+  Skeleton,
   Text,
   VStack,
-  // Wrap,
-  // WrapItem,
+  Wrap,
+  WrapItem,
 } from '@chakra-ui/react';
 import { CgList } from 'react-icons/cg';
 import { FaRegPlusSquare } from 'react-icons/fa';
 import { GoArrowSwitch } from 'react-icons/go';
 
 import { HomeIcon, VaultIcon } from '@/components';
+import { ExtraVaultCard, VaultCard } from '@/modules/vault';
 
+import { useHome } from '..';
 import { ActionCard } from '../components/ActionCard';
 
 const HomePage = () => {
+  const {
+    vaultsRequest: {
+      vaults: { recentVaults, extraCount, vaultsMax },
+      isLoading: loadingRecentVaults,
+    },
+    navigate,
+  } = useHome();
+
   return (
     <VStack w="full" spacing={6}>
       <HStack w="full" h="10" justifyContent="space-between">
@@ -37,8 +49,9 @@ const HomePage = () => {
           </Button>
         </Box>
       </HStack>
+
       <HStack spacing={6}>
-        <ActionCard.Container navigateTo="/predicate">
+        <ActionCard.Container onClick={() => navigate('/predicate')}>
           <ActionCard.Icon icon={VaultIcon} />
           <Box>
             <ActionCard.Title>Vaults</ActionCard.Title>
@@ -48,7 +61,8 @@ const HomePage = () => {
             </ActionCard.Description>
           </Box>
         </ActionCard.Container>
-        <ActionCard.Container navigateTo="/transaction">
+
+        <ActionCard.Container onClick={() => navigate('/transaction')}>
           <ActionCard.Icon icon={GoArrowSwitch} />
           <Box>
             <ActionCard.Title>Transactions</ActionCard.Title>
@@ -58,6 +72,7 @@ const HomePage = () => {
             </ActionCard.Description>
           </Box>
         </ActionCard.Container>
+
         <ActionCard.Container isUpcoming={true}>
           <ActionCard.Icon icon={CgList} isUpcoming={true} />
           <Box>
@@ -69,6 +84,45 @@ const HomePage = () => {
           </Box>
         </ActionCard.Container>
       </HStack>
+
+      <Box mt={4} alignSelf="flex-start">
+        <Heading variant="title-xl">Recently used vaults</Heading>
+      </Box>
+      <Wrap w="full" justifyContent="flex-start" spacing={0}>
+        {recentVaults?.map(
+          ({ id, name, predicateAddress, addresses }, index) => {
+            const lastCard = index === vaultsMax - 1;
+            const hasMore = extraCount > 0;
+
+            return (
+              <WrapItem w="25%" key={id}>
+                <Skeleton
+                  speed={1}
+                  startColor="dark.200"
+                  endColor="dark.500"
+                  isLoaded={!loadingRecentVaults}
+                  w="100%"
+                  borderRadius={10}
+                >
+                  {lastCard && hasMore ? (
+                    <ExtraVaultCard
+                      extra={extraCount}
+                      onClick={() => navigate('/predicate')}
+                    />
+                  ) : (
+                    <VaultCard
+                      name={name}
+                      address={predicateAddress}
+                      members={addresses}
+                      onClick={() => navigate(`/predicate/${id}`)}
+                    />
+                  )}
+                </Skeleton>
+              </WrapItem>
+            );
+          },
+        )}
+      </Wrap>
     </VStack>
   );
 };
