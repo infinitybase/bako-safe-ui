04edfd218c4e0f543c3fc60dcf7042674954c920
Pedro Barão <phbarao@gmail.com>
2023-10-04 21:59:10 -0300

    feat: update useHome hook

--- src/modules/home/hooks/useHome.ts
+++ src/modules/home/hooks/useHome.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useFuelAccount } from '@/modules';
@@ -6,14 +7,22 @@ import { useUserVaultRequest } from '@/modules/vault';
 const useHome = () => {
   const navigate = useNavigate();
   const { account } = useFuelAccount();
-  const request = useUserVaultRequest(account);
+  const vaultsRequest = useUserVaultRequest();
+
+  const vaults = useMemo(() => {
+    const max = 8;
+    const userPredicates = vaultsRequest.data;
+    const count = userPredicates?.length ?? 0;
+    const extraCount = count <= max ? 0 : count - max;
+    const recentVaults =
+      count <= max ? userPredicates : userPredicates?.slice(0, max);
+
+    return { recentVaults, extraCount, vaultsMax: max };
+  }, [vaultsRequest.data]);
 
   return {
     account,
-    request: {
-      ...request,
-      predicates: request.data,
-    },
+    vaultsRequest: { ...vaultsRequest, vaults },
     navigate,
   };
 };
