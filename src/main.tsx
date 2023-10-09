9763f367b2ef3364678301e75783d83379f87723
luisburigo <guga.burigo29@outlook.com>
2023-10-09 20:18:41 -0300

    chore: remove dev code

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { DevSupport } from '@react-buddy/ide-toolbox';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from '@/App';
 import { BsafeQueryClientProvider } from '@/config';
-import { ComponentPreviews, useInitial } from '@/dev';
 import { defaultTheme } from '@/themes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BsafeQueryClientProvider>
       <ChakraProvider theme={defaultTheme}>
-        <DevSupport
-          ComponentPreviews={ComponentPreviews}
-          useInitialHook={useInitial}
-        >
-          <App />
-        </DevSupport>
+        <App />
       </ChakraProvider>
     </BsafeQueryClientProvider>
   </React.StrictMode>,
