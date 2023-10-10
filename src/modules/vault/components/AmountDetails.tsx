208129952f999c4ac48233b47121ac20cf634e46
luisburigo <guga.burigo29@outlook.com>
2023-10-10 18:14:34 -0300

    chore: change vault asset list

--- src/modules/vault/components/AmountDetails.tsx
+++ src/modules/vault/components/AmountDetails.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Image, Text, VStack } from '@chakra-ui/react';
+import { Box, chakra, HStack, Image, Text, VStack } from '@chakra-ui/react';
 import React from 'react';
 
 import { Card } from '@/components';
@@ -15,77 +15,73 @@ export interface AmountDetailsProps {
   assets: UseVaultDetailsReturn['assets'];
 }
 
+const AssetCard = chakra(Card, {
+  baseStyle: {
+    w: 'full',
+    py: 5,
+    px: 6,
+    bgColor: 'dark.300',
+  },
+});
+
 const AmountDetails = (props: AmountDetailsProps) => {
   const { assets } = props;
   const isBig = assets?.value ? formatList(assets.value) : 0;
 
   return (
-    <Box w="full" maxW="25%">
+    <Box w="full" maxW="205px">
       <Box mb={5} w="full">
         <Text color="grey.200" fontWeight="semibold" fontSize="20px">
           Balance
         </Text>
       </Box>
-      {assets?.value &&
-        assets.value.map((asset: Asset, index: number) => {
-          if (isBig > 0 && index > 3) return;
-          if (isBig > 0 && index == 3) {
+      <VStack justifyContent="space-between">
+        {assets?.value &&
+          assets.value.map((asset: Asset, index: number) => {
+            if (isBig > 0 && index > 3) return;
+            if (isBig > 0 && index == 3) {
+              return (
+                <AssetCard key={index} w="full">
+                  <HStack
+                    w="100%"
+                    spacing={0}
+                    justifyContent="center"
+                    alignItems="center"
+                    display="flex"
+                    flexDirection="column"
+                    cursor="pointer"
+                  >
+                    <Text
+                      variant="description"
+                      fontSize="20px"
+                      fontWeight="bold"
+                    >
+                      +{isBig + 1}
+                    </Text>
+                    <Text variant="description" fontSize="15px">
+                      View all
+                    </Text>
+                  </HStack>
+                </AssetCard>
+              );
+            }
             return (
-              <Card
-                key={index}
-                w="full"
-                h="85"
-                p={2}
-                display="flex"
-                alignItems="center"
-                mb={2}
-                borderStyle="dashed"
-              >
-                <HStack
-                  w="100%"
-                  spacing={0}
-                  justifyContent="center"
-                  alignItems="center"
-                  display="flex"
-                  flexDirection="column"
-                  cursor="pointer"
-                >
-                  <Text variant="description" fontSize="20px" fontWeight="bold">
-                    +{isBig + 1}
-                  </Text>
-                  <Text variant="description" fontSize="15px">
-                    View all
-                  </Text>
+              <AssetCard key={index}>
+                <HStack w="full" spacing={4}>
+                  <Image src={assetsMap[asset.assetId].icon} boxSize="38px" />
+                  <Box>
+                    <Text color="grey.200" fontWeight="semibold" fontSize="lg">
+                      {asset.amount}
+                    </Text>
+                    <Text variant="description" fontSize="md">
+                      {assetsMap[asset.assetId].slug}
+                    </Text>
+                  </Box>
                 </HStack>
-              </Card>
+              </AssetCard>
             );
-          }
-          return (
-            <Card
-              key={index}
-              w="full"
-              h="85"
-              p={2}
-              display="flex"
-              alignItems="center"
-              mb={2}
-            >
-              <HStack w="30%" justifyContent="center">
-                <Image src={assetsMap[asset.assetId].icon} boxSize={12} />
-              </HStack>
-              <HStack w="70%" display="flex" justifyContent="flex-start">
-                <VStack justifyContent="flex-start" alignItems="start">
-                  <Text color="grey.200" fontWeight="semibold" fontSize="20px">
-                    {asset.amount}
-                  </Text>
-                  <Text variant="description" fontSize="20px">
-                    {assetsMap[asset.assetId].slug}
-                  </Text>
-                </VStack>
-              </HStack>
-            </Card>
-          );
-        })}
+          })}
+      </VStack>
     </Box>
   );
 };
