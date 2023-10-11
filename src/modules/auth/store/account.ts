56e35e9f8b8ec1217857222270f74ad11751e0b8
guimroque <guilhermemr_2017@hotmail.com>
2023-10-11 17:45:31 -0300

    feat(avatar): header profile

--- src/modules/auth/store/account.ts
+++ src/modules/auth/store/account.ts
@@ -6,13 +6,17 @@ import { AddressUtils } from '@/modules';
 interface State {
   account: string;
   formattedAccount: string;
+  avatar: string;
   setAccount: (account: string) => void;
+  setAvatar: (avatar: string) => void;
 }
-const { ADDRESS } = CookieName;
+const { ADDRESS, AVATAR } = CookieName;
 
 const useFuelAccount = create<State>((set) => ({
   account: CookiesConfig.getCookie(ADDRESS)!,
-  formattedAccount: AddressUtils.format(CookiesConfig.getCookie(ADDRESS)!),
+  formattedAccount: AddressUtils.format(CookiesConfig.getCookie(ADDRESS)!)!,
+  avatar: CookiesConfig.getCookie(AVATAR)!,
+  setAvatar: (avatar) => set({ avatar }),
   setAccount: (account) =>
     set({
       account,
