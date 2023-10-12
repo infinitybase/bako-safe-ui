b4015ccd58f2bba7589e9b7bebb0958bae7d4c07
luisburigo <guga.burigo29@outlook.com>
2023-10-12 01:40:08 -0300

    fix: import dependencies

--- src/modules/transactions/hooks/signature/useSignTransaction.ts
+++ src/modules/transactions/hooks/signature/useSignTransaction.ts
@@ -1,18 +1,22 @@
 import { useEffect, useMemo } from 'react';
 
+import { useFuelAccount } from '@/modules/auth';
 import {
   BsafeProvider,
   invalidateQueries,
   Transaction,
-  TRANSACTION_LIST_PAGINATION_QUERY_KEY,
-  TRANSACTION_LIST_QUERY_KEY,
   TransactionStatus,
-  useFuelAccount,
-  USER_TRANSACTIONS_QUERY_KEY,
-} from '@/modules';
-import { useToast, useWalletSignMessage } from '@/modules/core';
+  useToast,
+  useWalletSignMessage,
+} from '@/modules/core';
+import { VAULT_DETAIL_QUERY_KEY } from '@/modules/vault';
 
 import { useTransactionSendRequest } from '../details';
+import {
+  TRANSACTION_LIST_PAGINATION_QUERY_KEY,
+  TRANSACTION_LIST_QUERY_KEY,
+  USER_TRANSACTIONS_QUERY_KEY,
+} from '../list';
 import { useSignTransactionRequest } from './useSignTransactionRequest';
 
 export interface SignTransactionParams {
@@ -38,6 +42,7 @@ const useSignTransaction = (options: UseSignTransactionOptions) => {
       TRANSACTION_LIST_QUERY_KEY,
       TRANSACTION_LIST_PAGINATION_QUERY_KEY,
       USER_TRANSACTIONS_QUERY_KEY,
+      VAULT_DETAIL_QUERY_KEY,
     ]);
 
   const request = useSignTransactionRequest({
