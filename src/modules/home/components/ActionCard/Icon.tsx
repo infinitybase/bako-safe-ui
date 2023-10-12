ec191bb81b0eac789f37cc2407c6578b94a5e580
Pedro Barão <phbarao@gmail.com>
2023-10-11 21:03:32 -0300

    fix: action card icons

--- src/modules/home/components/ActionCard/Icon.tsx
+++ src/modules/home/components/ActionCard/Icon.tsx
@@ -1,4 +1,4 @@
-import { As, Box, Flex, Icon as ChakraIcon } from '@chakra-ui/react';
+import { As, Box, Center, Flex, Icon as ChakraIcon } from '@chakra-ui/react';
 
 interface ActionCardIconProps {
   icon: As;
@@ -7,7 +7,12 @@ interface ActionCardIconProps {
 
 const Icon = ({ icon, isUpcoming }: ActionCardIconProps) => {
   return (
-    <Flex alignItems="center" justifyContent="center" mr={3}>
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+      mr={3}
+      position="relative"
+    >
       <Box
         h="80px"
         w="80px"
@@ -15,12 +20,13 @@ const Icon = ({ icon, isUpcoming }: ActionCardIconProps) => {
         opacity={isUpcoming ? '0.2' : '0.1'}
         borderRadius={10}
       />
-      <ChakraIcon
-        fontSize={30}
-        as={icon}
-        position="absolute"
-        color={isUpcoming ? 'grey.500' : 'brand.500'}
-      />
+      <Center position="absolute">
+        <ChakraIcon
+          fontSize={30}
+          as={icon}
+          color={isUpcoming ? 'grey.500' : 'brand.500'}
+        />
+      </Center>
     </Flex>
   );
 };
