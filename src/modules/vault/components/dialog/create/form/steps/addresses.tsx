d0815197f3f816531814c30281a01a63a1b613f5
luisburigo <guga.burigo29@outlook.com>
2023-10-12 21:24:19 -0300

    chore: hidde template selection when not found

--- src/modules/vault/components/dialog/create/form/steps/addresses.tsx
+++ src/modules/vault/components/dialog/create/form/steps/addresses.tsx
@@ -37,6 +37,7 @@ const VaultAddressesStep = ({
 }: VaultAddressesStepProps) => (
   <TabPanel p={0}>
     <Dialog.Section
+      hidden={!templates.length}
       title={
         <Heading fontSize="lg" color="grey.200">
           Vault rules
@@ -46,9 +47,9 @@ const VaultAddressesStep = ({
       mb={8}
     />
 
-    <Divider borderColor="dark.100" mt={5} mb={9} />
+    <Divider hidden={!templates.length} borderColor="dark.100" mt={5} mb={9} />
 
-    <Box w="100%">
+    <Box hidden={!templates.length} w="100%">
       <FormControl>
         <Select
           placeholder=" "
@@ -71,7 +72,7 @@ const VaultAddressesStep = ({
       </FormControl>
     </Box>
 
-    <Divider borderColor="dark.100" my={9} />
+    <Divider hidden={!templates.length} borderColor="dark.100" my={9} />
 
     <Dialog.Section
       title={
