a44a2068d3969528b3529f54e51d20648e31a024
Pedro Barão <phbarao@gmail.com>
2023-10-18 20:17:09 -0300

    feat: add action on transactions button

--- src/modules/vault/pages/user-vaults/index.tsx
+++ src/modules/vault/pages/user-vaults/index.tsx
@@ -100,12 +100,12 @@ const UserVaultsPage = () => {
             </Box>
           </ActionCard.Container>
 
-          <ActionCard.Container isUpcoming={true}>
-            <ActionCard.Icon isUpcoming={true} icon={GoArrowSwitch} />
+          <ActionCard.Container
+            onClick={() => navigate(Pages.userTransactions())}
+          >
+            <ActionCard.Icon icon={GoArrowSwitch} />
             <Box>
-              <ActionCard.Title isUpcoming={true}>
-                Transactions
-              </ActionCard.Title>
+              <ActionCard.Title>Transactions</ActionCard.Title>
               <ActionCard.Description>
                 Manage Transactions Across All Vaults in One Place.
               </ActionCard.Description>
