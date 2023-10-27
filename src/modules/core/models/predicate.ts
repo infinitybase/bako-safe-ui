33e6ab051be9adf29b0fd14e579d66b81d449820
Pedro Barão <phbarao@gmail.com>
2023-10-26 23:23:32 -0300

    refactor(vault): update predicate types

--- src/modules/core/models/predicate.ts
+++ src/modules/core/models/predicate.ts
@@ -1,6 +1,18 @@
 import { Transaction } from './transaction';
 import { User } from './user';
 
+export interface PredicateMember {
+  id: string;
+  avatar: string;
+  address: string;
+  isOwner?: boolean;
+}
+
+export interface Owner {
+  id: string;
+  address: string;
+}
+
 export interface Predicate {
   id: string;
   name: string;
@@ -8,12 +20,13 @@ export interface Predicate {
   description: string;
   minSigners: number;
   completeAddress: User[];
-  addresses: string[];
-  owner: string;
+  addresses?: string[];
+  owner: Owner;
   bytes: string;
   abi: string;
   configurable: string;
   provider: string;
   chainId?: number;
+  members?: PredicateMember[];
   transactions: Transaction[];
 }
