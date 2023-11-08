78f7a7bd1740d1c109eb03d77e9b8b970a0430e8
Pedro Barão <phbarao@gmail.com>
2023-11-08 19:13:44 -0300

    feat(address-book): add routes and update exports

--- src/modules/addressBook/components/index.ts
+++ src/modules/addressBook/components/index.ts
@@ -1,1 +1,2 @@
+export * from './contactCard';
 export * from './dialog';
--- src/modules/addressBook/hooks/index.ts
+++ src/modules/addressBook/hooks/index.ts
@@ -1,4 +1,5 @@
-export * from './useContact';
+export * from './useAddressBook';
 export * from './useCreateContactForm';
 export * from './useCreateContactRequest';
 export * from './useFindContactsRequest';
+export * from './useListContactsRequest';
