05b4e41c744ebb9457b9b0bdf9038520eaeeb85e
Pedro Barão <phbarao@gmail.com>
2023-12-12 21:39:39 -0300

    refactor(notification): remove unused files

--- src/modules/notifications/components/filter/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NotificationsFilterControl } from './control';
-import { NotificationsFilterField } from './field';
-
-const NotificationsFilter = {
-  Control: NotificationsFilterControl,
-  Field: NotificationsFilterField,
-};
-
-export { NotificationsFilter };
