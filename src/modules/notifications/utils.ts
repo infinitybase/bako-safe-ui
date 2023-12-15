a05d09268287579f7527d900cee590cb5fed725d
Pedro Barão <phbarao@gmail.com>
2023-12-15 09:59:59 -0300

    fix(notification): add single quotes to vault's and transaction's names on notifications

--- src/modules/notifications/utils.ts
+++ src/modules/notifications/utils.ts
@@ -10,7 +10,7 @@ const notificationDescription = (
     [NotificationTitle.NEW_VAULT_CREATED]: `The '${vaultName}' has been created, and you are a signer!`,
     [NotificationTitle.TRANSACTION_CREATED]: `The transaction '${transactionName}' has been created on the '${vaultName}' vault.`,
     [NotificationTitle.TRANSACTION_SIGNED]: `The transaction '${transactionName}' has been signed in the '${vaultName}' vault.`,
-    [NotificationTitle.TRANSACTION_COMPLETED]: `The transaction ${transactionName} has been completed in the ${vaultName} vault.`,
+    [NotificationTitle.TRANSACTION_COMPLETED]: `The transaction '${transactionName}' has been completed in the '${vaultName}' vault.`,
     [NotificationTitle.TRANSACTION_DECLINED]: `The transaction '${transactionName}' has been declined in the '${vaultName}' vault.`,
   };
 
