46ac3d37c6b29db5a76e0f3fd3861463258b1c4d
luisburigo <guga.burigo29@outlook.com>
2023-12-18 16:41:07 -0300

    ci: add mode in vite to build with environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,15 @@ import * as path from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const mode = process.env.VERCEL_ENV || 'development';
+
+if (mode !== 'development') {
+  console.log('[BUILD] Env:', mode);
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  mode,
   plugins: [react(), tsconfigPaths()],
   cacheDir: '.vite',
   resolve: {
