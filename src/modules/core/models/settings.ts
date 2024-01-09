6e7428c7c04ffc1a1ef8774a47a83499699baf96
Pedro Barão <phbarao@gmail.com>
2024-01-09 18:45:20 -0300

    feat(notifications): add method and request

---
+++ src/modules/core/models/settings.ts
@@ -0,0 +1,3 @@
+export enum SettingsQueryKey {
+  MY_SETTINGS = 'settings/me',
+}
---
+++ src/modules/settings/hooks/useMySettingsRequest.ts
@@ -0,0 +1,13 @@
+import { useQuery } from 'react-query';
+
+import { SettingsQueryKey } from '@/modules/core';
+
+import { SettingsService } from '../services';
+
+const useMySettingsRequest = (account: string) => {
+  return useQuery([SettingsQueryKey.MY_SETTINGS, account], async () =>
+    SettingsService.getSettings(),
+  );
+};
+
+export { useMySettingsRequest };
