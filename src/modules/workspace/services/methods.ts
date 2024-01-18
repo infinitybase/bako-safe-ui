b17684214a8ee1bf83695c447b02c21935c82676
luisburigo <guga.burigo29@outlook.com>
2024-01-18 10:57:52 -0300

    feat(workspace): method to create workspace in api

--- src/modules/workspace/services/methods.ts
+++ src/modules/workspace/services/methods.ts
@@ -1,12 +1,30 @@
 import { api } from '@/config';
-import { Workspace } from '@/modules/core';
+import { IPermissions, Workspace } from '@/modules/core';
+
+export interface CreateWorkspacePayload {
+  name: string;
+  members?: string[];
+  description?: string;
+  avatar?: string;
+  single?: boolean;
+  permissions?: IPermissions;
+}
 
 export type ListUserWorkspacesResponse = Workspace[];
+export type CreateWorkspaceResponse = Workspace;
 
 export class WorkspaceService {
   static async list(userAddress: string) {
     const { data } = await api.get<ListUserWorkspacesResponse>(
       `/workspace/by-user/${userAddress}`,
+    );
+
+    return data;
+  }
+  static async create(payload: CreateWorkspacePayload) {
+    const { data } = await api.post<CreateWorkspaceResponse>(
+      `/workspace`,
+      payload,
     );
 
     return data;
