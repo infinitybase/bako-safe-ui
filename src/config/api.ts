24444833b69d21900527c2d6b0b8dcf391a2569f
Pedro Barão <phbarao@gmail.com>
2024-01-17 21:41:18 -0300

    feat(auth): clear cookies on logout

--- src/config/api.ts
+++ src/config/api.ts
@@ -5,7 +5,8 @@ import { useFuelAccount } from '@/modules';
 import { CookieName, CookiesConfig } from './cookies';
 
 const { VITE_API_URL } = import.meta.env;
-const { ACCESS_TOKEN, ADDRESS } = CookieName;
+const { ACCESS_TOKEN, ADDRESS, AVATAR, USER_ID, WORKSPACE, PERMISSIONS } =
+  CookieName;
 
 export enum ApiUnauthorizedErrorsTitles {
   MISSING_CREDENTIALS = 'Missing credentials',
@@ -53,8 +54,12 @@ api.interceptors.response.use(
     if (unauthorizedError) {
       useFuelAccount.getState().setAccount('');
       CookiesConfig.removeCookies([
-        CookieName.ACCESS_TOKEN,
-        CookieName.ADDRESS,
+        ACCESS_TOKEN,
+        ADDRESS,
+        AVATAR,
+        USER_ID,
+        WORKSPACE,
+        PERMISSIONS,
       ]);
     }
 
