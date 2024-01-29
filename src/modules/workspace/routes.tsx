08ff8df101a6d52306c2178809e0cbcfa6baf21e
luisburigo <guga.burigo29@outlook.com>
2024-01-29 18:53:25 -0300

    feat(workspace): fix workspace routes import

--- src/modules/workspace/routes.tsx
+++ src/modules/workspace/routes.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import { Pages } from '@/modules/core';
+import { DashboardLayoutRouter } from '@/layouts';
+import { AuthRoute, Pages } from '@/modules/core';
 
 import { CreateMemberPage, CreateWorkspacePage } from './pages';
 import { WorkspacePage } from './pages/home';
-
-const AuthRoute = React.lazy(async () => {
-  const newVar = await import('@/modules/core');
-  return { default: newVar.AuthRoute };
-});
-
-const DashboardLayoutRouter = React.lazy(async () => {
-  const newVar = await import('@/layouts');
-  return { default: newVar.DashboardLayoutRouter };
-});
 
 const workspaceRoutes = (
   <Route>
