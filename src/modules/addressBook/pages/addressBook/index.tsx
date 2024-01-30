088f6e291dac2874b09e685f223efe9fbf99730f
Pedro Barão <phbarao@gmail.com>
2024-01-30 15:26:10 -0300

    fix(address-book): redirects on breadcrumbs and backHome button

--- src/modules/addressBook/pages/addressBook/index.tsx
+++ src/modules/addressBook/pages/addressBook/index.tsx
@@ -101,7 +101,7 @@ const AddressBookPage = () => {
                   fontSize="sm"
                   color="grey.200"
                   fontWeight="semibold"
-                  href={Pages.home()}
+                  onClick={() => navigate(Pages.home())}
                 >
                   Home
                 </BreadcrumbLink>
@@ -112,7 +112,11 @@ const AddressBookPage = () => {
                   fontSize="sm"
                   color="grey.200"
                   fontWeight="semibold"
-                  href={Pages.workspace({ workspaceId: currentWorkspace.id })}
+                  onClick={() =>
+                    navigate(
+                      Pages.workspace({ workspaceId: currentWorkspace.id }),
+                    )
+                  }
                 >
                   {currentWorkspace.name}
                 </BreadcrumbLink>
