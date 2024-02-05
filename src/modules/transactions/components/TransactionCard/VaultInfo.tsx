4a7bf6d376b59ddddf385907027f7125f9ac74d0
Pedro Barão <phbarao@gmail.com>
2024-02-05 09:46:59 -0300

    feat(transaction): add workspace name on transactions card

--- src/modules/transactions/components/TransactionCard/VaultInfo.tsx
+++ src/modules/transactions/components/TransactionCard/VaultInfo.tsx
@@ -1,46 +1,47 @@
+import { Icon } from '@chakra-ui/icons';
 import { Avatar, Heading, HStack, Text, VStack } from '@chakra-ui/react';
 
+import { HandbagIcon } from '@/components/icons/handbag';
 import { Predicate } from '@/modules/core';
 
 interface TransactionVaultInfoProps {
   vault: Predicate;
 }
 
-const VaultInfo = ({ vault }: TransactionVaultInfoProps) => (
-  <HStack w={235}>
-    <Avatar
-      variant="roundedSquare"
-      name={vault.name}
-      color="white"
-      bg="grey.900"
-      w="38px"
-      h="38px"
-      p={5}
-      fontSize="sm"
-    />
-    <VStack ml={1} alignItems="flex-start" spacing={0}>
-      <Heading
-        maxW={180}
-        variant="title-md"
-        color="grey.200"
-        mt={0}
-        isTruncated
-      >
-        {vault.name}
-      </Heading>
-      {vault?.description && (
-        <Text
+const VaultInfo = ({ vault }: TransactionVaultInfoProps) => {
+  return (
+    <HStack w={235}>
+      <Avatar
+        variant="roundedSquare"
+        name={vault.name}
+        color="white"
+        bg="grey.900"
+        w="38px"
+        h="38px"
+        p={5}
+        fontSize="sm"
+      />
+      <VStack ml={1} alignItems="flex-start" spacing={0}>
+        {!vault.workspace.single && (
+          <HStack>
+            <Icon as={HandbagIcon} fontSize="md" color="grey.200" />
+            <Text maxW={40} color="grey.200" fontSize="sm" isTruncated>
+              {vault.workspace?.name}
+            </Text>
+          </HStack>
+        )}
+        <Heading
           maxW={180}
-          variant="description"
-          fontSize="sm"
-          color="grey.500"
+          variant="title-md"
+          color="grey.200"
+          mt={0}
           isTruncated
         >
-          {vault.description}
-        </Text>
-      )}
-    </VStack>
-  </HStack>
-);
+          {vault.name}
+        </Heading>
+      </VStack>
+    </HStack>
+  );
+};
 
 export { VaultInfo };
