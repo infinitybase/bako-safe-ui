2407a9d4c049607b6f72ed2fe6cadf7192d2d5c7
pedroanastacio <pedro.anastacio013@gmail.com>
2024-02-26 20:48:16 -0300

    feat(auth): signin page is now responsive

--- src/modules/auth/pages/signin.tsx
+++ src/modules/auth/pages/signin.tsx
@@ -3,13 +3,19 @@ import { Box, Button, Text } from '@chakra-ui/react';
 import { useMemo } from 'react';
 
 import { useContactToast } from '@/modules/addressBook';
-import { DrawerConnector, SigninContainer } from '@/modules/auth/components';
+import {
+  DrawerConnector,
+  SigninContainer,
+  SigninContainerMobile,
+} from '@/modules/auth/components';
+import { useScreenSize } from '@/modules/core';
 
 import { useSignIn } from '../hooks';
 
 const SigninPage = () => {
   const { isConnecting, connectors, redirectToWalletLink, auth } = useSignIn();
   const { errorToast } = useContactToast();
+  const { isMobile } = useScreenSize();
 
   useMemo(() => {
     auth.isInvalidAccount &&
@@ -22,11 +28,14 @@ const SigninPage = () => {
 
   const pageSections = {
     description: connectors.has
-      ? 'Click the button bellow to connect your wallet to BSAFE.'
+      ? 'Click the button bellow to connect Bako Safe.'
       : 'You need to use the fuel wallet to connect.',
     action: connectors.has ? (
       <Button
-        size="lg"
+        size={{
+          base: 'md',
+          sm: 'lg',
+        }}
         color="dark.500"
         fontWeight="bold"
         variant="solid"
@@ -61,6 +70,46 @@ const SigninPage = () => {
     ),
   };
 
+  if (isMobile) {
+    return (
+      <SigninContainerMobile>
+        <DrawerConnector
+          isOpen={connectors.drawer.isOpen}
+          onClose={connectors.drawer.onClose}
+          onSelect={connectors.select}
+          connectors={connectors.items}
+        />
+        <Box
+          w="full"
+          backgroundColor="brand.500"
+          bgGradient="linear(to-br, brand.500 , brand.800)"
+          borderRadius="10px 10px 0px 0px"
+          p={6}
+          pl="40%"
+        >
+          <Box textAlign="end" mb={3}>
+            <Text
+              fontSize="2xl"
+              fontWeight="bold"
+              bgClip="text"
+              color="dark.300"
+            >
+              Hello,
+            </Text>
+          </Box>
+          <Box textAlign="end">
+            <Text fontSize="small" fontWeight="semibold" color="dark.300">
+              {pageSections.description}
+            </Text>
+          </Box>
+        </Box>
+        <Box display="flex" justifyContent="flex-end" w="full" p={6}>
+          {pageSections.action}
+        </Box>
+      </SigninContainerMobile>
+    );
+  }
+
   return (
     <SigninContainer>
       <DrawerConnector
@@ -76,7 +125,7 @@ const SigninPage = () => {
           bgGradient="linear(to-r, brand.500, brand.800)"
           bgClip="text"
         >
-          Hey!
+          Hello,
         </Text>
       </Box>
       <Box textAlign="start" mb={5} maxW={305}>
