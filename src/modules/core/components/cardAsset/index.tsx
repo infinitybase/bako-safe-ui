922d91ecb6c3ee8d920a4d91da819062fb69acd7
pedroanastacio <pedro.anastacio013@gmail.com>
2024-02-28 08:26:04 -0300

    feat(core): make asset card responsive

--- src/modules/core/components/cardAsset/index.tsx
+++ src/modules/core/components/cardAsset/index.tsx
@@ -1,13 +1,55 @@
-import { Avatar, Card, CardProps, Text, VStack } from '@chakra-ui/react';
+import {
+  Avatar,
+  Box,
+  Card,
+  CardProps,
+  HStack,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 
+import { useScreenSize } from '../../hooks';
 import { Asset, assetsMap, NativeAssetId } from '../../utils';
+
+interface DefaultAsset {
+  assetId: string;
+  amount: string;
+  name: string;
+  slug: string;
+  icon?: string | undefined;
+}
+
+interface AssetDetailsProps {
+  asset: Asset;
+  defaultAsset: DefaultAsset;
+}
 
 interface AssetCardProps extends CardProps {
   asset: Asset;
   visibleBalance?: boolean;
 }
 
+const AssetDetails = ({ asset, defaultAsset }: AssetDetailsProps) => {
+  return (
+    <Box>
+      <Text color="grey.100" fontSize={{ base: 'sm', sm: 15 }} noOfLines={2}>
+        {asset.name ?? defaultAsset.name}
+      </Text>
+
+      <Text
+        fontWeight="bold"
+        fontSize={{ base: 'sm', sm: 15 }}
+        color="grey.400"
+      >
+        {asset.slug ?? defaultAsset.slug}
+      </Text>
+    </Box>
+  );
+};
+
 const AssetCard = ({ asset, visibleBalance, ...rest }: AssetCardProps) => {
+  const { isMobile } = useScreenSize();
+
   const defaultAsset = {
     ...assetsMap[NativeAssetId],
     assetId: NativeAssetId,
@@ -21,19 +63,22 @@ const AssetCard = ({ asset, visibleBalance, ...rest }: AssetCardProps) => {
       borderColor="grey.400"
       borderWidth="2px"
       borderRadius={10}
-      px={6}
+      px={{ base: 4, sm: 6 }}
       py={4}
       w="full"
-      h={150}
+      h={{ base: undefined, sm: 150 }}
       {...rest}
     >
-      <Avatar
-        w={10}
-        h={10}
-        mb={3}
-        variant="roundedSquare"
-        src={asset.icon ?? defaultAsset.icon}
-      />
+      <HStack gap={2} alignItems="center" mb={{ base: 1, sm: 4 }}>
+        <Avatar
+          w={{ base: 8, sm: 10 }}
+          h={{ base: 8, sm: 10 }}
+          variant="roundedSquare"
+          src={asset.icon ?? defaultAsset.icon}
+        />
+
+        {isMobile && <AssetDetails asset={asset} defaultAsset={defaultAsset} />}
+      </HStack>
 
       <VStack
         display="flex"
@@ -44,7 +89,13 @@ const AssetCard = ({ asset, visibleBalance, ...rest }: AssetCardProps) => {
         gap={-1}
       >
         {visibleBalance ? (
-          <Text fontWeight="bold" color="white" maxW={360} isTruncated>
+          <Text
+            fontSize={{ base: 'sm', sm: 'md' }}
+            fontWeight="bold"
+            color="white"
+            maxW={360}
+            isTruncated
+          >
             {asset.amount ?? defaultAsset.amount}
           </Text>
         ) : (
@@ -53,13 +104,9 @@ const AssetCard = ({ asset, visibleBalance, ...rest }: AssetCardProps) => {
           </Text>
         )}
 
-        <Text color="grey.100" mt={1} fontSize={15} noOfLines={2}>
-          {asset.name ?? defaultAsset.name}
-        </Text>
-
-        <Text fontWeight="bold" fontSize={15} color="grey.400">
-          {asset.slug ?? defaultAsset.slug}
-        </Text>
+        {!isMobile && (
+          <AssetDetails asset={asset} defaultAsset={defaultAsset} />
+        )}
       </VStack>
     </Card>
   );
