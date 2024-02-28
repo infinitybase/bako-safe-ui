f0c6018a9f003adbd17640ab11e90e8ee71198b9
Jon Ventura <jonathan_vmaced@hotmail.com>
2024-02-28 18:54:27 -0300

    chore: added export to index

--- src/modules/core/hooks/index.ts
+++ src/modules/core/hooks/index.ts
@@ -3,5 +3,6 @@ export * from './fuel';
 export * from './socket';
 export * from './useIsMount';
 export * from './useLoadImage';
+export * from './usePermissions';
 export * from './useTab';
 export * from './useToast';
