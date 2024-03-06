5cbeef25384b8da91cce753cbd38e635660a877b
Jon Ventura <jonathan_vmaced@hotmail.com>
2024-03-06 20:20:39 -0300

    feat(transaction): adjusted stepper

--- src/modules/transactions/components/TransactionCard/TransactionStepper.tsx
+++ src/modules/transactions/components/TransactionCard/TransactionStepper.tsx
@@ -2,54 +2,86 @@ import {
   Box,
   Step,
   StepDescription,
+  StepIcon,
   StepIndicator,
   Stepper,
   StepSeparator,
+  StepStatus,
   StepTitle,
+  Text,
 } from '@chakra-ui/react';
 
-import { ITransactionHistory } from '../../services';
+import { ITransactionHistory, TransactionHistoryType } from '../../services';
 
 interface TransactionStepperProps {
   steps: ITransactionHistory[];
 }
 
+const TransactionTypeFormatter = (type: string) => {
+  switch (type) {
+    case TransactionHistoryType.CREATED:
+      return 'Created';
+    case TransactionHistoryType.SEND:
+      return 'Sent';
+    case TransactionHistoryType.SIGN:
+      return 'Signed';
+  }
+};
+
 const TransactionStepper = ({ steps }: TransactionStepperProps) => {
   return (
-    <Stepper
-      index={steps?.length}
-      orientation="vertical"
-      h="240px"
-      size="xs"
-      maxH="full"
-      gap="0"
-      colorScheme="success"
-      w="full"
-    >
-      {steps?.map((step, index) => (
-        <Step key={index}>
-          <StepIndicator rounded={5} />
+    <Box display="flex" flexDirection="column" gap={5}>
+      <Text color="grey.200" fontWeight="medium">
+        TransactionHistory
+      </Text>
 
-          <Box>
-            <StepTitle
-              style={{
-                fontSize: '16px',
-              }}
-            >
-              {step.type}
-            </StepTitle>
-            <StepDescription
-              style={{
-                fontSize: '14px',
-              }}
-            >
-              {new Date(step.date).toString().split(' ').slice(0, 5).join(' ')}
-            </StepDescription>
-          </Box>
-          <StepSeparator />
-        </Step>
-      ))}
-    </Stepper>
+      <Stepper
+        index={4}
+        orientation="vertical"
+        h="220px"
+        size="sm"
+        maxH="full"
+        gap={0}
+        colorScheme="success"
+      >
+        {steps?.map((step, index) => (
+          <Step
+            key={index}
+            style={{
+              display: 'flex',
+              alignItems: 'flex-start',
+              justifyContent: 'center',
+            }}
+          >
+            <StepIndicator bg="grey.400" rounded={5}>
+              <StepStatus key={index} complete={<StepIcon color="black" />} />
+            </StepIndicator>
+
+            <StepSeparator />
+            <Box>
+              <StepTitle
+                style={{
+                  fontSize: '16px',
+                }}
+              >
+                {TransactionTypeFormatter(step.type)}
+              </StepTitle>
+              <StepDescription
+                style={{
+                  fontSize: '14px',
+                }}
+              >
+                {new Date(step.date)
+                  .toString()
+                  .split(' ')
+                  .slice(0, 5)
+                  .join(' ')}
+              </StepDescription>
+            </Box>
+          </Step>
+        ))}
+      </Stepper>
+    </Box>
   );
 };
 
