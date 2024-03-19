b04b87bf4edcc160c34c5cf9217eca3da2ec8f66
Jon Ventura <jonathan_vmaced@hotmail.com>
2024-03-19 20:46:01 -0300

    fix(vault): adjusted vault by name endpoint

--- src/modules/vault/hooks/useGetByNameVaultRequest.ts
+++ src/modules/vault/hooks/useGetByNameVaultRequest.ts
@@ -1,15 +1,13 @@
 import { useQuery } from 'react-query';
-import { useParams } from 'react-router-dom';
 
 import { VaultService } from '../services';
 
 const VAULT_LIST_QUERY_KEY = 'predicate/by-name';
 
 const useCheckVaultName = (name: string) => {
-  const params = useParams<{ workspaceId: string }>();
   return useQuery(
     [VAULT_LIST_QUERY_KEY, name],
-    () => VaultService.getByName(params.workspaceId!, name),
+    () => VaultService.getByName(name),
     {
       refetchOnWindowFocus: false,
     },
