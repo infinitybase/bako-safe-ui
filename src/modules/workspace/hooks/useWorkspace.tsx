17acfce449d6f25cb71bbbcacb218e39b5a4570a
guimroque <guilhermemr_2017@hotmail.com>
2024-04-12 15:48:01 -0300

    fix: build

--- src/modules/workspace/hooks/useWorkspace.tsx
+++ src/modules/workspace/hooks/useWorkspace.tsx
@@ -7,6 +7,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { useAuth } from '@/modules/auth/hooks/useAuth';
 import { useHomeDataRequest } from '@/modules/home/hooks/useHomeDataRequest';
 import { useNotification } from '@/modules/notification';
+import { useTransactionsSignaturePending } from '@/modules/transactions/hooks/list';
 
 import { Pages } from '../../core';
 import { PermissionRoles } from '../../core/models';
@@ -32,6 +33,7 @@ const useWorkspace = () => {
 
   const workspaceHomeRequest = useHomeDataRequest();
   const userWorkspacesRequest = useUserWorkspacesRequest();
+  const pendingSignerTransactions = useTransactionsSignaturePending();
 
   const worksapceBalance = useGetWorkspaceBalanceRequest();
   const workspaceRequest = useGetCurrentWorkspace();
@@ -124,6 +126,7 @@ const useWorkspace = () => {
     visibleBalance,
     setVisibleBalance,
     goWorkspace,
+    pendingSignerTransactions,
   };
 };
 
