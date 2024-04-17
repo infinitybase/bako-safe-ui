2b4790f8da716f59fedbd896e7d7fffe16f574a0
Rafael <rafael-dev@hotmail.com>
2024-04-17 12:59:25 -0300

    fix(workspace): adjust admin to create vault on vault list and transaction list

--- src/modules/vault/pages/user-vaults/index.tsx
+++ src/modules/vault/pages/user-vaults/index.tsx
@@ -118,7 +118,7 @@ const UserVaultsPage = () => {
             variant="primary"
             fontWeight="bold"
             leftIcon={<FaRegPlusSquare />}
-            isDisabled={!hasPermission([OWNER, MANAGER])}
+            isDisabled={!hasPermission([OWNER, MANAGER, ADMIN])}
             onClick={() =>
               navigate(
                 Pages.createVault({
