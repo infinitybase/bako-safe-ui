7948eea3d6492d1c6ba37b0116412e96ce0aaa87
pedroanastacio <pedro.anastacio013@gmail.com>
2024-04-26 09:58:31 -0300

    feat: add small size to screen size hook

--- src/modules/core/hooks/useScreenSize.ts
+++ src/modules/core/hooks/useScreenSize.ts
@@ -1,14 +1,16 @@
 import { useMediaQuery } from '@chakra-ui/react';
 
 const useScreenSize = () => {
-  const [isMobile] = useMediaQuery('(max-width: 62em)');
-  const [isExtraSmall] = useMediaQuery('(max-width: 21em)'); //336 px
+  const [isMobile] = useMediaQuery('(max-width: 62em)'); //992px
+  const [isSmall] = useMediaQuery('(max-width: 37.48em)'); // 600px
+  const [isExtraSmall] = useMediaQuery('(max-width: 21em)'); //336px
   const [vaultRequiredSizeToColumnLayout] = useMediaQuery(
     '(max-width: 75.62em)',
   ); //1029px
 
   return {
     isMobile,
+    isSmall,
     vaultRequiredSizeToColumnLayout,
     isExtraSmall,
   };
