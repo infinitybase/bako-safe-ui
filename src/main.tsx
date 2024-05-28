fab14e43360e39b7c1344dc7f4bb2c4bf3708001
guimroque <guilhermemr_2017@hotmail.com>
2024-05-28 16:32:08 -0300

    fix: devnet

--- src/main.tsx
+++ src/main.tsx
@@ -27,7 +27,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={fuelConnectorsQueryClient}>
         <FuelProvider
           fuelConfig={{
-            connectors: defaultConnectors({ devMode: import.meta.env.DEV }),
+            connectors: defaultConnectors({
+              devMode: true,
+            }),
           }}
         >
           <SocketProvider>
