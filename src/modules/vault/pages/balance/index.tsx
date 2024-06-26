9d43e0f83a6a678e2eb311dc03d311381e77f795
pedroanastacio <pedro.anastacio013@gmail.com>
2024-06-26 14:13:16 -0300

    feat(vault): add balance page to vault routes

---
+++ src/modules/vault/pages/balance/index.tsx
@@ -0,0 +1,120 @@
+import {
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  HStack,
+  Icon,
+  Text,
+} from '@chakra-ui/react';
+import { RiMenuUnfoldLine } from 'react-icons/ri';
+
+import { HomeIcon } from '@/components';
+import { Drawer } from '@/layouts/dashboard/drawer';
+import { useAuth } from '@/modules/auth';
+import { Pages, useScreenSize } from '@/modules/core';
+import { useHome } from '@/modules/home';
+import { useGetCurrentWorkspace, useWorkspace } from '@/modules/workspace';
+
+import { useVaultDetails } from '../../hooks';
+
+const VaultBalancePage = () => {
+  const { vault, navigate, menuDrawer } = useVaultDetails();
+  const { goWorkspace } = useWorkspace();
+  const { workspace } = useGetCurrentWorkspace();
+  const {
+    workspaces: { current },
+    isSingleWorkspace,
+  } = useAuth();
+  const { goHome } = useHome();
+  const { vaultRequiredSizeToColumnLayout } = useScreenSize();
+
+  if (!vault) return null;
+
+  return (
+    <Box w="full">
+      <Drawer isOpen={menuDrawer.isOpen} onClose={menuDrawer.onClose} />
+
+      <HStack mb={8} w="full" justifyContent="space-between">
+        {vaultRequiredSizeToColumnLayout ? (
+          <HStack gap={1.5} onClick={menuDrawer.onOpen}>
+            <Icon as={RiMenuUnfoldLine} fontSize="xl" color="grey.200" />
+            <Text fontSize="sm" fontWeight="normal" color="grey.100">
+              Menu
+            </Text>
+          </HStack>
+        ) : (
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <BreadcrumbLink
+                fontSize="sm"
+                color="grey.200"
+                fontWeight="semibold"
+                onClick={() => goHome()}
+              >
+                <Icon mr={2} as={HomeIcon} fontSize="sm" color="grey.200" />
+                Home
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+
+            {!isSingleWorkspace && (
+              <BreadcrumbItem>
+                <BreadcrumbLink
+                  fontSize="sm"
+                  color="grey.200"
+                  fontWeight="semibold"
+                  onClick={() => goWorkspace(current)}
+                  maxW={40}
+                  isTruncated
+                >
+                  {workspace?.name}
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            )}
+            <BreadcrumbItem>
+              <BreadcrumbLink
+                fontSize="sm"
+                color="grey.200"
+                fontWeight="semibold"
+                href="#"
+              >
+                Vaults
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <BreadcrumbLink
+                fontSize="sm"
+                color="grey.200"
+                fontWeight="semibold"
+                onClick={() =>
+                  navigate(
+                    Pages.detailsVault({
+                      vaultId: vault.id!,
+                      workspaceId: current ?? '',
+                    }),
+                  )
+                }
+                isTruncated
+                maxW={640}
+              >
+                {vault.name}
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <BreadcrumbLink
+                fontSize="sm"
+                color="grey.200"
+                fontWeight="semibold"
+                href="#"
+              >
+                Balance
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>
+        )}
+      </HStack>
+    </Box>
+  );
+};
+
+export { VaultBalancePage };
