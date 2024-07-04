3e6f1ddc2191a44fd281e79ac69b64b15501b999
pedroanastacio <pedro.anastacio013@gmail.com>
2024-07-04 18:07:52 -0300

    feat(cli): create centralized hook to handle api tokens

---
+++ src/modules/cli/hooks/APIToken/useAPIToken.ts
@@ -0,0 +1,125 @@
+import { useDisclosure } from '@chakra-ui/react';
+import { useMemo, useState } from 'react';
+
+import { useCreateAPIToken } from './create';
+import { useListAPITokens } from './list';
+import { useRemoveAPIToken } from './remove';
+
+export enum TabState {
+  LIST,
+  CREATE,
+  SUCCESS,
+}
+
+export type UseAPITokenReturn = ReturnType<typeof useAPIToken>;
+
+const useAPIToken = () => {
+  const [tab, setTab] = useState<TabState>(TabState.LIST);
+
+  const dialog = useDisclosure();
+
+  const {
+    form,
+    request: createRequest,
+    createdAPIKey,
+  } = useCreateAPIToken(setTab);
+  const { request: listRequest } = useListAPITokens();
+  const { confirm, request: removeRequest, handler } = useRemoveAPIToken();
+
+  const hasToken = useMemo(() => {
+    return listRequest.data && listRequest.data?.length > 0;
+  }, [listRequest]);
+
+  const handleCloseDialog = () => {
+    setTab(TabState.LIST);
+    dialog.onClose();
+    createdAPIKey.set('');
+    form.reset();
+  };
+
+  const stepsActions = {
+    [TabState.LIST]: {
+      title: 'API Tokens',
+      description:
+        'Send single or batch payments with multi assets. You can send multiple types of assets to different addresses.',
+      primaryAction: {
+        handler: () => {},
+        label: '',
+        disabled: true,
+        hide: true,
+        isLoading: false,
+      },
+      secondaryAction: {
+        handler: handleCloseDialog,
+        label: 'Done',
+      },
+      hideHeader: false,
+    },
+    [TabState.CREATE]: {
+      title: 'Create new API Tokens',
+      description:
+        'Define the name and description of this vault. These details will be visible to all members.',
+      primaryAction: {
+        handler: () => form.submit(),
+        label: 'Next',
+        disabled: false,
+        hide: false,
+        isLoading: createRequest.isLoading,
+      },
+      secondaryAction: {
+        handler: handleCloseDialog,
+        label: 'Cancel',
+      },
+      hideHeader: false,
+    },
+    [TabState.SUCCESS]: {
+      title: 'API Token created!',
+      description:
+        'You will not have another chance to copy this keys. Make sure you are saving in a safe place',
+      primaryAction: {
+        handler: () => {},
+        label: 'Done',
+        disabled: false,
+        hide: true,
+        isLoading: false,
+      },
+      secondaryAction: {
+        handler: handleCloseDialog,
+        label: 'Done',
+      },
+      hideHeader: true,
+    },
+  };
+
+  return {
+    dialog: {
+      ...dialog,
+      onClose: handleCloseDialog,
+    },
+    steps: {
+      step: stepsActions[tab],
+      actions: stepsActions,
+    },
+    tabs: {
+      tab,
+      set: setTab,
+    },
+    list: {
+      request: listRequest,
+    },
+    create: {
+      request: createRequest,
+      dialog,
+      form,
+      createdAPIKey,
+    },
+    remove: {
+      confirm,
+      request: removeRequest,
+      handler,
+    },
+    hasToken,
+  };
+};
+
+export { useAPIToken };
