49d60f2cf16dc08cff6cf7b277180ba2ee60337a
guimroque <guilhermemr_2017@hotmail.com>
2024-07-10 22:44:41 -0300

    fix: pending request

--- src/modules/transactions/hooks/list/useTotalSignaturesPendingRequest.ts
+++ src/modules/transactions/hooks/list/useTotalSignaturesPendingRequest.ts
@@ -5,9 +5,17 @@ import { TransactionService } from '../../services';
 const PENDING_TRANSACTIONS_QUERY_KEY = 'pending-transactions';
 
 const useTransactionsSignaturePending = (predicateId?: string[]) => {
-  return useQuery([PENDING_TRANSACTIONS_QUERY_KEY, predicateId], () => {
-    return TransactionService.getTransactionsSignaturePending(predicateId);
-  });
+  return useQuery(
+    [PENDING_TRANSACTIONS_QUERY_KEY, predicateId],
+    () => {
+      return TransactionService.getTransactionsSignaturePending(predicateId);
+    },
+    {
+      cacheTime: 1000 * 60 * 4,
+      staleTime: 1000 * 60 * 4,
+      refetchInterval: 1000 * 60 * 4,
+    },
+  );
 };
 
 export { PENDING_TRANSACTIONS_QUERY_KEY, useTransactionsSignaturePending };
