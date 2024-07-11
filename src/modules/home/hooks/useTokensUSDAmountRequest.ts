df4b8a78a1c036154c6088d44fb2ca2a351a963f
guimroque <guilhermemr_2017@hotmail.com>
2024-07-10 22:28:23 -0300

    chore: token request

--- src/modules/home/hooks/useTokensUSDAmountRequest.ts
+++ src/modules/home/hooks/useTokensUSDAmountRequest.ts
@@ -2,13 +2,18 @@ import { useQuery } from 'react-query';
 
 import { HomeService } from '../services';
 import { useTokensStore } from '@/modules/assets-tokens/store/useTokensStore';
+import { useAuth } from '@/modules/auth';
 
 const useTokensUSDAmountRequest = () => {
   const { setTokenCurrentAmount, setIsLoading } = useTokensStore();
+  const { account } = useAuth();
 
   return useQuery(['tokens'], () => HomeService.getTokensUSDAmount(), {
     refetchOnWindowFocus: false,
     refetchInterval: 1000 * 60 * 10,
+    cacheTime: 1000 * 60 * 10,
+    staleTime: 1000 * 60 * 10,
+    enabled: !!account,
     onSuccess: (data) => {
       setTokenCurrentAmount(data ?? []);
       setIsLoading(false);
