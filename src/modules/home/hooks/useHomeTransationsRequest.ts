2380ad60aa92a9f7f4f85b5da92de6754e88a41b
guimroque <guilhermemr_2017@hotmail.com>
2024-07-10 23:07:22 -0300

    chore: tx cache

--- src/modules/home/hooks/useHomeTransationsRequest.ts
+++ src/modules/home/hooks/useHomeTransationsRequest.ts
@@ -14,6 +14,8 @@ const useHomeTransactionsRequest = (type: TransactionType | undefined) => {
     () => HomeService.homeTransactions(type),
     {
       refetchOnWindowFocus: true,
+      cacheTime: 1000 * 60 * 4,
+      staleTime: 1000 * 60 * 4,
     },
   );
 };
