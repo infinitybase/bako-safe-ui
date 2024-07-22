d14274a56ad55dbf98c7d6a9b7eef41d64fe9975
Rafael <rafael-dev@hotmail.com>
2024-07-22 18:26:52 -0300

    fix(vault): fixed types

--- src/modules/vault/routes.tsx
+++ src/modules/vault/routes.tsx
@@ -9,50 +9,50 @@ import { VaultInfosProvider } from './providers/VaultInfosProvider';
 
 const VaultRoutes = () => (
   <VaultInfosProvider>
-    <Routes>
-      <Route element={<DashboardLayoutRouter hasSideBar />}>
-        <Route
-          path={Pages.detailsVault()}
-          element={
-            <AuthRoute>
-              <VaultDetailsPage />
-            </AuthRoute>
-          }
-        />
-        <Route
-          path={Pages.createTemplate()}
-          element={
-            <AuthRoute>
-              <CreateTemplatePage />
-            </AuthRoute>
-          }
-        />
-        <Route
-          path={Pages.createTransaction()}
-          element={
-            <AuthRoute>
-              <CreateTransactionPage />
-            </AuthRoute>
-          }
-        />
-        <Route
-          path={Pages.vaultSettings()}
-          element={
-            <AuthRoute>
-              <VaultSettingsPage />
-            </AuthRoute>
-          }
-        />
-        <Route
-          path={Pages.vaultBalance()}
-          element={
-            <AuthRoute>
-              <VaultBalancePage />
-            </AuthRoute>
-          }
-        />
-      </Route>
-    </Routes>
+    {/* <Routes> */}
+    <Route element={<DashboardLayoutRouter hasSideBar />}>
+      <Route
+        path={Pages.detailsVault()}
+        element={
+          <AuthRoute>
+            <VaultDetailsPage />
+          </AuthRoute>
+        }
+      />
+      <Route
+        path={Pages.createTemplate()}
+        element={
+          <AuthRoute>
+            <CreateTemplatePage />
+          </AuthRoute>
+        }
+      />
+      <Route
+        path={Pages.createTransaction()}
+        element={
+          <AuthRoute>
+            <CreateTransactionPage />
+          </AuthRoute>
+        }
+      />
+      <Route
+        path={Pages.vaultSettings()}
+        element={
+          <AuthRoute>
+            <VaultSettingsPage />
+          </AuthRoute>
+        }
+      />
+      <Route
+        path={Pages.vaultBalance()}
+        element={
+          <AuthRoute>
+            <VaultBalancePage />
+          </AuthRoute>
+        }
+      />
+    </Route>
+    {/* </Routes> */}
   </VaultInfosProvider>
 );
 
