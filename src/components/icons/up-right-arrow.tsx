5e6054c3577f7aaa085dc4d778254aa4abba381b
pedroanastacio <pedro.anastacio013@gmail.com>
2024-08-14 19:44:37 -0300

    chore(icons): change icon names and add dynamic color

--- src/components/icons/up-right-arrow.tsx
+++ src/components/icons/up-right-arrow.tsx
@@ -2,18 +2,18 @@ import { createIcon } from '@chakra-ui/react';
 
 const UpRightArrow = createIcon({
   displayName: 'UpRightArrow',
-  viewBox: '0 0 9 9',
+  viewBox: '0 0 16 16',
   path: (
     <svg
-      width="9"
-      height="9"
-      viewBox="0 0 9 9"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M7.66895 2.776L1.93095 8.514L0.988281 7.57133L6.72561 1.83333H1.66895V0.5H9.00228V7.83333H7.66895V2.776Z"
-        fill="#FFC010"
+        d="M10.6689 6.276L4.93095 12.014L3.98828 11.0713L9.72561 5.33333H4.66895V4H12.0023V11.3333H10.6689V6.276Z"
+        fill="currentColor"
       />
     </svg>
   ),
