9d92247a242634e70738d835e423ecec45fba22f
pedroanastacio <pedro.anastacio013@gmail.com>
2024-08-22 17:03:41 -0300

    chore(icons): replace notify icon

--- src/components/icons/notify-icon.tsx
+++ src/components/icons/notify-icon.tsx
@@ -4,116 +4,114 @@ const NotifyIcon = createIcon({
   displayName: 'NotifyIcon',
   viewBox: '0 0 97 97',
   path: (
-    <>
-      <svg
-        width="97"
-        height="96"
-        viewBox="0 0 97 96"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <g filter="url(#filter0_d_18046_72295)">
-          <rect
-            x="8.5"
-            width="80"
-            height="80"
-            rx="8"
-            fill="url(#paint0_linear_18046_72295)"
-            fill-opacity="0.1"
-            shape-rendering="crispEdges"
+    <svg
+      width="97"
+      height="96"
+      viewBox="0 0 97 96"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_d_18911_135218)">
+        <rect
+          x="8.5"
+          width="80"
+          height="80"
+          rx="8"
+          fill="url(#paint0_linear_18911_135218)"
+          fillOpacity="0.1"
+          shapeRendering="crispEdges"
+        />
+        <rect
+          x="9"
+          y="0.5"
+          width="79"
+          height="79"
+          rx="7.5"
+          stroke="url(#paint1_linear_18911_135218)"
+          shapeRendering="crispEdges"
+        />
+        <path
+          d="M53.9002 52.3324C52.8803 54.7718 50.9407 56.7114 48.5013 57.7313C46.0619 56.7114 44.1223 54.7718 43.1024 52.3324H46.5866C47.0891 53.0863 47.7382 53.7384 48.5013 54.2487C49.2644 53.7399 49.9135 53.0863 50.4176 52.3324H53.9002ZM57.7513 44.3234L60.8346 47.8199V50.7907H36.168V47.8199L39.2513 44.3234V35.3741C39.2513 30.0044 43.1116 25.4349 48.5013 23.7422C53.891 25.4349 57.7513 30.0044 57.7513 35.3741V44.3234ZM56.6259 47.7074L54.668 45.4874V35.3741C54.668 31.8005 52.2476 28.5445 48.5013 27.0182C44.7551 28.5445 42.3346 31.7989 42.3346 35.3741V45.4874L40.3767 47.7074H56.6259ZM48.5013 38.4574C47.6836 38.4574 46.8993 38.1325 46.3211 37.5543C45.7428 36.9761 45.418 36.1918 45.418 35.3741C45.418 34.5563 45.7428 33.7721 46.3211 33.1938C46.8993 32.6156 47.6836 32.2907 48.5013 32.2907C49.3191 32.2907 50.1033 32.6156 50.6815 33.1938C51.2598 33.7721 51.5846 34.5563 51.5846 35.3741C51.5846 36.1918 51.2598 36.9761 50.6815 37.5543C50.1033 38.1325 49.3191 38.4574 48.5013 38.4574Z"
+          fill="url(#paint2_linear_18911_135218)"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_18911_135218"
+          x="0.5"
+          y="0"
+          width="96"
+          height="96"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
           />
-          <rect
-            x="9"
-            y="0.5"
-            width="79"
-            height="79"
-            rx="7.5"
-            stroke="url(#paint1_linear_18046_72295)"
-            shape-rendering="crispEdges"
+          <feOffset dy="8" />
+          <feGaussianBlur stdDeviation="4" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
           />
-          <path
-            d="M56.4002 52.3324C55.3803 54.7718 53.4407 56.7114 51.0013 57.7313C48.5619 56.7114 46.6223 54.7718 45.6024 52.3324H49.0866C49.5891 53.0863 50.2382 53.7384 51.0013 54.2487C51.7644 53.7399 52.4135 53.0863 52.9176 52.3324H56.4002ZM60.2513 44.3234L63.3346 47.8199V50.7907H38.668V47.8199L41.7513 44.3234V35.3741C41.7513 30.0044 45.6116 25.4349 51.0013 23.7422C56.391 25.4349 60.2513 30.0044 60.2513 35.3741V44.3234ZM59.1259 47.7074L57.168 45.4874V35.3741C57.168 31.8005 54.7476 28.5445 51.0013 27.0182C47.2551 28.5445 44.8346 31.7989 44.8346 35.3741V45.4874L42.8767 47.7074H59.1259ZM51.0013 38.4574C50.1836 38.4574 49.3993 38.1325 48.8211 37.5543C48.2428 36.9761 47.918 36.1918 47.918 35.3741C47.918 34.5563 48.2428 33.7721 48.8211 33.1938C49.3993 32.6156 50.1836 32.2907 51.0013 32.2907C51.8191 32.2907 52.6033 32.6156 53.1815 33.1938C53.7598 33.7721 54.0846 34.5563 54.0846 35.3741C54.0846 36.1918 53.7598 36.9761 53.1815 37.5543C52.6033 38.1325 51.8191 38.4574 51.0013 38.4574Z"
-            fill="url(#paint2_linear_18046_72295)"
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_18911_135218"
           />
-        </g>
-        <defs>
-          <filter
-            id="filter0_d_18046_72295"
-            x="0.5"
-            y="0"
-            width="96"
-            height="96"
-            filterUnits="userSpaceOnUse"
-            color-interpolation-filters="sRGB"
-          >
-            <feFlood flood-opacity="0" result="BackgroundImageFix" />
-            <feColorMatrix
-              in="SourceAlpha"
-              type="matrix"
-              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-              result="hardAlpha"
-            />
-            <feOffset dy="8" />
-            <feGaussianBlur stdDeviation="4" />
-            <feComposite in2="hardAlpha" operator="out" />
-            <feColorMatrix
-              type="matrix"
-              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-            />
-            <feBlend
-              mode="normal"
-              in2="BackgroundImageFix"
-              result="effect1_dropShadow_18046_72295"
-            />
-            <feBlend
-              mode="normal"
-              in="SourceGraphic"
-              in2="effect1_dropShadow_18046_72295"
-              result="shape"
-            />
-          </filter>
-          <linearGradient
-            id="paint0_linear_18046_72295"
-            x1="8.5"
-            y1="0"
-            x2="92.2257"
-            y2="75.8896"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#FFC010" />
-            <stop offset="0.48" stop-color="#EBA312" />
-            <stop offset="0.71" stop-color="#D38015" />
-            <stop offset="0.99" stop-color="#B24F18" />
-          </linearGradient>
-          <linearGradient
-            id="paint1_linear_18046_72295"
-            x1="8.5"
-            y1="0"
-            x2="92.2257"
-            y2="75.8896"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#FFC010" />
-            <stop offset="0.48" stop-color="#EBA312" />
-            <stop offset="0.71" stop-color="#D38015" />
-            <stop offset="0.99" stop-color="#B24F18" />
-          </linearGradient>
-          <linearGradient
-            id="paint2_linear_18046_72295"
-            x1="38.668"
-            y1="23.7422"
-            x2="71.4904"
-            y2="45.3328"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#FFC010" />
-            <stop offset="0.48" stop-color="#EBA312" />
-            <stop offset="0.71" stop-color="#D38015" />
-            <stop offset="0.99" stop-color="#B24F18" />
-          </linearGradient>
-        </defs>
-      </svg>
-    </>
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_18911_135218"
+            result="shape"
+          />
+        </filter>
+        <linearGradient
+          id="paint0_linear_18911_135218"
+          x1="8.5"
+          y1="0"
+          x2="92.2257"
+          y2="75.8896"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFC010" />
+          <stop offset="0.48" stopColor="#EBA312" />
+          <stop offset="0.71" stopColor="#D38015" />
+          <stop offset="0.99" stopColor="#B24F18" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_18911_135218"
+          x1="8.5"
+          y1="0"
+          x2="92.2257"
+          y2="75.8896"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFC010" />
+          <stop offset="0.48" stopColor="#EBA312" />
+          <stop offset="0.71" stopColor="#D38015" />
+          <stop offset="0.99" stopColor="#B24F18" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_18911_135218"
+          x1="36.168"
+          y1="23.7422"
+          x2="68.9904"
+          y2="45.3328"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFC010" />
+          <stop offset="0.48" stopColor="#EBA312" />
+          <stop offset="0.71" stopColor="#D38015" />
+          <stop offset="0.99" stopColor="#B24F18" />
+        </linearGradient>
+      </defs>
+    </svg>
   ),
 });
 
