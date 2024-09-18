830ca71848215343a5bdaf8e5446076b2d8c3067
pedroanastacio <pedro.anastacio013@gmail.com>
2024-09-18 15:06:14 -0300

    chore(sign-in): create sign in page exclusive to web

--- src/modules/auth/pages/signin.tsx
+++ src/modules/auth/pages/signin.tsx
@@ -1,143 +1,33 @@
-import { TabPanel, TabPanels, Tabs, VStack } from '@chakra-ui/react';
-import { useEffect } from 'react';
+import { SignInWrapper } from '@/modules/auth/components';
 
-import { useWalletSignIn, useWebAuthnSignIn } from '@/modules';
-import { useContactToast } from '@/modules/addressBook';
-import {
-  ConnectorsList,
-  SigninContainer,
-  SigninContainerMobile,
-  SignInFooter,
-  SignInHeader,
-  WebAuthnAccountCreated,
-  WebAuthnSignIn,
-} from '@/modules/auth/components';
-import { useListConnectors } from '@/modules/core/hooks/fuel/useListConnectors';
-import { useWorkspaceContext } from '@/modules/workspace/WorkspaceProvider';
+import { useWebSignIn } from '../hooks/signIn/useWebSignIn';
 
-const SigninPage = () => {
-  const { connectors } = useListConnectors();
-  const { handleSelectWallet, isAnyWalletConnectorOpen } = useWalletSignIn();
+const WebSignInPage = () => {
   const {
+    isAnyWalletConnectorOpen,
     tabs,
     formData,
     formState,
     accountsOptions,
     createdAcccountUsername,
     inputBadge,
+    handleSelectWallet,
     handleInputChange,
-  } = useWebAuthnSignIn();
-  const { errorToast } = useContactToast();
-  const {
-    authDetails: auth,
-    screenSizes: { isMobile },
-  } = useWorkspaceContext();
-
-  const pageSections = {
-    title: 'Welcome to Bako Safe',
-  };
-
-  useEffect(() => {
-    auth.invalidAccount &&
-      errorToast({
-        title: 'Invalid Account',
-        description: 'You need to use the fuel wallet to connect.',
-      });
-    auth.handlers.setInvalidAccount?.(false);
-  }, [auth.invalidAccount]);
-
-  if (isMobile) {
-    return (
-      <SigninContainerMobile>
-        <Tabs index={tabs.tab} flex={1} w="full" display="flex">
-          <TabPanels flex={1}>
-            <TabPanel h="full" p={0}>
-              <VStack
-                justifyContent="center"
-                h="full"
-                w="full"
-                pt={20}
-                pb={6}
-                px={6}
-                spacing={14}
-              >
-                <SignInHeader title={pageSections.title} />
-
-                <VStack w="full" maxW={390} spacing={6}>
-                  <WebAuthnSignIn
-                    formData={formData}
-                    formState={formState}
-                    accountsOptions={accountsOptions}
-                    inputBadge={inputBadge}
-                    handleInputChange={handleInputChange}
-                  />
-
-                  <ConnectorsList
-                    connectors={connectors}
-                    onConnectorSelect={handleSelectWallet}
-                    isAnyWalletConnectorOpen={isAnyWalletConnectorOpen}
-                  />
-                </VStack>
-
-                <SignInFooter />
-              </VStack>
-            </TabPanel>
-
-            <TabPanel h="full">
-              <WebAuthnAccountCreated
-                title={createdAcccountUsername}
-                formState={formState}
-              />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </SigninContainerMobile>
-    );
-  }
+  } = useWebSignIn();
 
   return (
-    <SigninContainer>
-      <Tabs index={tabs.tab} flex={1} w="full">
-        <TabPanels h="full">
-          <TabPanel h="full" p={0}>
-            <VStack
-              h="full"
-              spacing={20}
-              alignItems="center"
-              justifyContent="center"
-            >
-              <SignInHeader title={pageSections.title} />
-
-              <VStack w="full" spacing={8} maxW={390}>
-                <WebAuthnSignIn
-                  formData={formData}
-                  formState={formState}
-                  accountsOptions={accountsOptions}
-                  inputBadge={inputBadge}
-                  handleInputChange={handleInputChange}
-                />
-
-                <ConnectorsList
-                  connectors={connectors}
-                  onConnectorSelect={handleSelectWallet}
-                  isAnyWalletConnectorOpen={isAnyWalletConnectorOpen}
-                />
-              </VStack>
-
-              <SignInFooter />
-            </VStack>
-          </TabPanel>
-
-          <TabPanel h="full">
-            <WebAuthnAccountCreated
-              title={createdAcccountUsername}
-              formState={formState}
-            />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </SigninContainer>
+    <SignInWrapper
+      tabs={tabs}
+      formData={formData}
+      formState={formState}
+      accountsOptions={accountsOptions}
+      inputBadge={inputBadge}
+      createdAcccountUsername={createdAcccountUsername}
+      isAnyWalletConnectorOpen={isAnyWalletConnectorOpen}
+      handleInputChange={handleInputChange}
+      handleSelectWallet={handleSelectWallet}
+    />
   );
 };
 
-export { SigninPage };
+export { WebSignInPage };
