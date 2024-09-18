51b6e4941528a76d3c1bc48c210ad4a60aaf8c60
pedroanastacio <pedro.anastacio013@gmail.com>
2024-09-18 15:04:47 -0300

    chore(sign-in): create component to centralize sign components

--- src/modules/auth/components/index.ts
+++ src/modules/auth/components/index.ts
@@ -4,4 +4,5 @@ export * from './footer';
 export * from './header';
 export * from './redirectAuthUser';
 export * from './redirectFromLandingPage';
+export * from './signInWrapper';
 export * from './webAuthn';
