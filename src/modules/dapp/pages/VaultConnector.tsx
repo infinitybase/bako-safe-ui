7708d532876ff053c316f9d147cecdcb75abf509
pedroanastacio <pedro.anastacio013@gmail.com>
2024-09-20 19:55:26 -0300

    refactor(connector): use requesting from component on vault select page

--- src/modules/dapp/pages/VaultConnector.tsx
+++ src/modules/dapp/pages/VaultConnector.tsx
@@ -1,5 +1,4 @@
 import {
-  Avatar,
   Box,
   Button,
   Card,
@@ -24,6 +23,7 @@ import { useVaultDrawer } from '@/modules/vault/components/modal/hook';
 import { WorkspacePermissionUtils } from '@/modules/workspace/utils';
 import { useWorkspaceContext } from '@/modules/workspace/WorkspaceProvider';
 
+import { DappTransaction } from '../components';
 import { useAuthSocket, useVerifyBrowserType } from '../hooks';
 
 const VaultConnector = () => {
@@ -98,42 +98,11 @@ const VaultConnector = () => {
               scrollbarWidth: 'none',
             }}
           >
-            {/* Requester */}
-            <Card
-              bgColor="dark.500"
-              borderColor="grey.925"
-              borderRadius={8}
-              p={4}
-              borderWidth="1px"
+            <DappTransaction.RequestingFrom
               mb={2}
-              w="full"
-            >
-              <Text fontSize={12} color="grey.550">
-                Requesting a transaction from:
-              </Text>
-
-              <Divider borderColor="grey.950" my={3} />
-
-              <HStack width="100%" spacing={3.5}>
-                <Avatar
-                  variant="roundedSquare"
-                  color="white"
-                  bgColor="dark.950"
-                  size="sm"
-                  borderRadius="6.4px"
-                  name={name!}
-                />
-                <VStack alignItems="flex-start" spacing={0}>
-                  <Text color="grey.250" fontSize="sm" fontWeight="semibold">
-                    {name}
-                  </Text>
-                  <Text color="brand.500" fontSize="xs" lineHeight={4}>
-                    {origin?.split('//')[1]}
-                    {/* fuel-connectors-hx60ddh96-fuel-labs.vercel.app */}
-                  </Text>
-                </VStack>
-              </HStack>
-            </Card>
+              name={name}
+              origin={origin}
+            />
 
             {vaults?.map(
               ({ id, name, predicateAddress, workspace, members, root }) => {
