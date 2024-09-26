93feedb505eeb15485ead2d519f64d458815c19b
guimroque <guilhermemr_2017@hotmail.com>
2024-09-25 22:38:53 -0300

    feat: fuel tokens

--- src/modules/core/utils/assets/data.ts
+++ src/modules/core/utils/assets/data.ts
@@ -1,70 +1,73 @@
-import {
-  BitcoinIcon,
-  EthereumIcon,
-  UniswapIcon,
-  UnknownIcon,
-  UsdcIcon,
-} from '@/components/icons';
-import { BakoIcon } from '@/components/icons/assets/bakoIcon';
-
-import { tokensIDS } from './address';
+import { assets } from 'fuels';
 import { Asset, AssetMap } from './types';
 const ETHDefault = 'https://cdn.fuel.network/assets/eth.svg';
 const NativeAssetId =
   '0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07';
 
-const assetsList: Asset[] = [
-  {
-    name: 'Ethereum',
-    slug: 'ETH',
-    assetId: NativeAssetId,
-    icon: EthereumIcon,
-  },
-  {
-    name: 'Bitcoin',
-    slug: 'BTC',
-    assetId: tokensIDS['BTC'],
-    icon: BitcoinIcon,
-  },
-  {
-    name: 'USDC',
-    slug: 'USDC',
-    assetId: tokensIDS['USDC'],
-    icon: UsdcIcon,
-  },
-  {
-    name: 'Uniswap',
-    slug: 'UNI',
-    assetId: tokensIDS['UNI'],
-    icon: UniswapIcon,
-  },
-  {
-    name: 'Dai',
-    slug: 'DAI',
-    assetId: tokensIDS['DAI'],
-    icon: EthereumIcon,
-  },
-  {
-    name: 'sEther',
-    slug: 'sETH',
-    assetId: tokensIDS['sETH'],
-    icon: EthereumIcon,
-  },
-  {
-    name: 'Unknown',
-    slug: 'UNK',
-    assetId: 'UNKNOWN',
-    icon: UnknownIcon,
-  },
-  {
-    name: 'BAKO',
-    slug: 'BAKO',
-    assetId: tokensIDS['BAKO'],
-    icon: BakoIcon,
-  },
-];
+//   {
+//     "name": "Ethereum",
+//     "symbol": "ETH",
+//     "icon": "https://cdn.fuel.network/assets/eth.svg",
+//     "networks": [
+//         {
+//             "type": "ethereum",
+//             "chainId": 11155111,
+//             "decimals": 18
+//         },
+//         {
+//             "type": "ethereum",
+//             "chainId": 31337,
+//             "decimals": 18
+//         },
+//         {
+//             "type": "ethereum",
+//             "chainId": 1,
+//             "decimals": 18
+//         },
+//         {
+//             "type": "fuel",
+//             "chainId": 0,
+//             "decimals": 9,
+//             "assetId": "0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07"
+//         },
+//         {
+//             "type": "fuel",
+//             "chainId": 0,
+//             "decimals": 9,
+//             "assetId": "0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07"
+//         },
+//         {
+//             "type": "fuel",
+//             "chainId": 9889,
+//             "decimals": 9,
+//             "assetId": "0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07"
+//         }
+//     ]
+// }
 
-const assetsMap: AssetMap = assetsList.reduce((previousValue, currentValue) => {
+export const formatedAssets = (chainId: number): Asset[] =>
+  assets.reduce<Asset[]>((acc, asset) => {
+    const network = asset.networks.find(
+      (network) => network && network.chainId === chainId,
+    );
+    if (network && network.type === 'fuel') {
+      acc.push({
+        name: asset.name,
+        slug: asset.symbol,
+        assetId: network.assetId,
+        icon: asset.icon,
+      });
+    }
+    return acc;
+  }, []);
+
+const assetsList: Asset[] = formatedAssets(
+  Number(import.meta.env.VITE_CHAIN_ID),
+);
+
+const assetsMap: AssetMap = formatedAssets(
+  Number(import.meta.env.VITE_CHAIN_ID),
+).reduce((previousValue, currentValue) => {
   return {
     ...previousValue,
     [currentValue.assetId]: {
