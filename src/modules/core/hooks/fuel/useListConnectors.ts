993b276976b9e86bbc20acfe290e6775e959b8e7
Pedro Barão <phbarao@gmail.com>
2024-09-27 18:41:21 -0300

    fix: build problem

--- src/modules/core/hooks/fuel/useListConnectors.ts
+++ src/modules/core/hooks/fuel/useListConnectors.ts
@@ -11,7 +11,7 @@ export enum EConnectors {
 
 export const EConnectorsInverse: Record<EConnectors, keyof typeof TypeUser> = {
   'Fuel Wallet': 'FUEL',
-  'Fuelet Wallet': 'FULLET',
+  'Fuelet Wallet': 'FUEL',
 };
 
 export enum EConnectorsLabels {
