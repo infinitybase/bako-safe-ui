fadd870a407643adb3c645626a2a55b9b95e2054
luisburigo <guga.burigo29@outlook.com>
2024-10-07 15:54:09 -0300

    chore: select network on create

--- src/modules/network/hooks/useNetworks.ts
+++ src/modules/network/hooks/useNetworks.ts
@@ -67,7 +67,7 @@ const useNetworks = (onClose?: () => void) => {
       const name = provider.getChain()?.name;
       const chainId = provider.getChainId();
 
-      NetworkService.create({
+      await NetworkService.create({
         name,
         url: url!,
         chainId,
@@ -93,10 +93,9 @@ const useNetworks = (onClose?: () => void) => {
       {
         onSuccess: async () => {
           setMode(NetworkDrawerMode.SELECT);
-          onClose?.();
+          await handleSelectNetwork(data.url);
           setValidNetwork(false);
           refetchNetworks();
-          handleSelectNetwork(data.url);
         },
       },
     );
