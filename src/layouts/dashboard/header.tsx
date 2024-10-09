8484c20acf6d44cf841b6d2cf62bfd8b35e43170
Pedro Barão <phbarao@gmail.com>
2024-10-09 19:05:51 -0300

    style: increase network selector width

--- src/layouts/dashboard/header.tsx
+++ src/layouts/dashboard/header.tsx
@@ -156,7 +156,7 @@ const UserBox = () => {
         >
           <PopoverTrigger>
             <HStack
-              w={165}
+              w={220}
               h={'32px'}
               alignItems="center"
               cursor={isWebAuthn ? 'pointer' : 'default'}
@@ -212,7 +212,7 @@ const UserBox = () => {
 
           <PopoverContent
             bg={'dark.300'}
-            w={165}
+            w={220}
             borderTop="none"
             border="1px solid #353230"
             _focus={{ ring: 'none' }}
