4fc8d8d45e7586126331398ac8c66a5427ff25d4
Rafael <rafael-dev@hotmail.com>
2024-10-11 14:38:16 -0300

    chore(dapp): create intermediate screen and redirect user after create a tx

--- src/utils/enviroment.ts
+++ src/utils/enviroment.ts
@@ -1,12 +1,32 @@
-export const getEnviroment = (): string => {
+const getEnvironment = (): string => {
   let bakoEnv = 'preview';
-  if (window.location.host.indexOf('stg-safe') >= 0) {
+
+  if (window.location.host.includes('stg-safe')) {
     bakoEnv = 'staging';
-  } else if (window.location.host.indexOf('hmg-safe') >= 0) {
+  } else if (window.location.host.includes('hmg-safe')) {
     bakoEnv = 'homologacao';
-  } else if (window.location.host.indexOf('safe.bako.global') >= 0) {
+  } else if (window.location.host.includes('safe.bako.global')) {
     bakoEnv = 'production';
   }
 
   return bakoEnv;
 };
+export const getUIUrl = (): string => {
+  const environment = getEnvironment();
+  let baseUrl = '';
+
+  switch (environment) {
+    case 'staging':
+      baseUrl = 'https://bsafe-ui-git-staging-infinity-base.vercel.app/';
+      break;
+    case 'production':
+      baseUrl = 'https://safe.bako.global/';
+      break;
+    default:
+      baseUrl = 'http://127.0.0.1:5174';
+  }
+
+  return baseUrl;
+};
+
+export { getEnvironment };
