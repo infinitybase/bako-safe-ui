5e21f076149e6faa65384e24e5d4645b36e8ab61
pedroanastacio <pedro.anastacio013@gmail.com>
2024-10-11 19:45:05 -0300

    feat(utils): create function to format asset amount using units

---
+++ src/utils/format-asset-amount.ts
@@ -0,0 +1,32 @@
+import { Assets, bn, BNInput, NetworkFuel } from 'fuels';
+
+import { Maybe } from '@/modules/core/utils';
+
+interface GetAssetUnitsParams {
+  fuelsTokens?: Assets;
+  chainId: number;
+  assetId: string;
+  amount: Maybe<BNInput>;
+}
+
+const formatAssetAmount = ({
+  fuelsTokens,
+  chainId,
+  assetId,
+  amount,
+}: GetAssetUnitsParams): string => {
+  const units =
+    fuelsTokens
+      ?.map((asset) => {
+        const network = asset.networks.find(
+          (network) => network && network.chainId === chainId,
+        ) as NetworkFuel;
+
+        if (network && network.assetId === assetId) return network.decimals;
+      })
+      .find((units) => units !== undefined) ?? 8;
+
+  return bn(amount).format({ units });
+};
+
+export { formatAssetAmount };
