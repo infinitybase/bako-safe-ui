81a7154846a91bc63076200bf3ef4dbc88b83bea
Rafael <rafael-dev@hotmail.com>
2024-10-17 11:34:53 -0300

    chore(create-tx): removed required validation on tx name

--- src/modules/transactions/components/dialog/create/index.tsx
+++ src/modules/transactions/components/dialog/create/index.tsx
@@ -66,6 +66,8 @@ const CreateTransactionDialog = (props: Omit<DialogModalProps, 'children'>) => {
     !transactionFee ||
     Number(transactionFee) === 0;
 
+  console.log('form.formState.isValid', form.formState.isValid);
+
   const isDisabled =
     !form.formState.isValid ||
     form.formState.isSubmitting ||
