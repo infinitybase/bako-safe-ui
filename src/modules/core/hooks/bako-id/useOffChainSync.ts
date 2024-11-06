ec39bc96792110ddd3edb78b96fa65334eff56d9
pedroanastacio <pedro.anastacio013@gmail.com>
2024-11-06 10:07:05 -0300

    feat(off-chain-sync): synchronizes every 10 minutes

--- src/modules/core/hooks/bako-id/useOffChainSync.ts
+++ src/modules/core/hooks/bako-id/useOffChainSync.ts
@@ -1,10 +1,27 @@
 import { OffChainSync } from '@bako-id/sdk';
+import { useQuery } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
 
 import { OffChainSyncInstance } from '../../utils/offChainSync';
 
+const OFF_CHAIN_SYNC_DATA_QUERY_KEY = 'off-chain-sync-data';
+
 const useOffChainSync = (networkUrl: string) => {
-  const [offChainSync, setOffChainSync] = useState<OffChainSync | null>(null);
+  const [offChainSync, setOffChainSync] = useState<OffChainSync | undefined>(
+    undefined,
+  );
+
+  useQuery({
+    queryKey: [OFF_CHAIN_SYNC_DATA_QUERY_KEY],
+    queryFn: async () => {
+      await offChainSync?.syncData();
+      return null;
+    },
+    refetchInterval: 10 * 60 * 1000, // 10m
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    staleTime: 500, // 500ms second to prevent request spam
+  });
 
   useEffect(() => {
     const initOffChainSync = async () => {
@@ -18,4 +35,4 @@ const useOffChainSync = (networkUrl: string) => {
   return offChainSync;
 };
 
-export { useOffChainSync };
+export { OFF_CHAIN_SYNC_DATA_QUERY_KEY, useOffChainSync };
