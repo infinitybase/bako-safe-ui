313dad1053556156b569e92a6cb5012b561d6e0d
pedroanastacio <pedro.anastacio013@gmail.com>
2024-11-06 17:22:15 -0300

    feat(address-book): creates function to autocomplete works with handles

---
+++ src/modules/addressBook/utils/index.ts
@@ -0,0 +1,1 @@
+export * from './sync-adb-autocomplete-option';
---
+++ src/modules/addressBook/utils/sync-adb-autocomplete-option.ts
@@ -0,0 +1,36 @@
+import { isValidDomain, OffChainSync } from '@bako-id/sdk';
+
+import { AutocompleteOption } from '@/components/autocomplete';
+import { AddressUtils } from '@/modules/core/utils/address';
+import { AddressBookUtils } from '@/utils/address-book';
+
+export const syncAddressBookAutocompleteOption = (
+  value: string,
+  sync?: OffChainSync,
+): AutocompleteOption | null => {
+  if (!sync) return null;
+
+  if (value.startsWith('@') && isValidDomain(value)) {
+    const resolver = sync?.getResolver(value.slice(1));
+
+    if (resolver) {
+      return {
+        value: resolver,
+        label: AddressBookUtils.formatForAutocomplete(value, resolver),
+      };
+    }
+  }
+
+  if (AddressUtils.isValid(value)) {
+    const domain = sync?.getDomain(value);
+
+    if (domain) {
+      return {
+        value,
+        label: AddressBookUtils.formatForAutocomplete(`@${domain}`, value),
+      };
+    }
+  }
+
+  return null;
+};
