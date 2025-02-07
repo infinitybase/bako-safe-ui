3638638f6e1249c64850d01e0b6211e8796a8526
luisburigo <gugaba29@gmail.com>
2025-02-07 17:26:38 -0300

    fix(vault): prevent overflow on vault dialog scroll

--- src/modules/vault/components/dialog/create/form/steps/addresses.tsx
+++ src/modules/vault/components/dialog/create/form/steps/addresses.tsx
@@ -113,7 +113,6 @@ const VaultAddressesStep = (props: VaultAddressesStepProps) => {
       <TabPanel p={0} maxH={500}>
         <VStack
           w="full"
-          flex={1}
           overflowY="scroll"
           sx={{
             '&::-webkit-scrollbar': {
@@ -127,7 +126,7 @@ const VaultAddressesStep = (props: VaultAddressesStepProps) => {
               height: '10px',
             },
           }}
-          h={{ base: '100%', xs: 500 }}
+          h={{ base: '60vh', xs: 500 }}
         >
           <CreateVaultWarning
             mb={2}
