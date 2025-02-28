9bd69a59779501975beff9a04f640cc982354dbc
vitor <pereiravitor1218@gmail.com>
2025-02-28 11:53:49 -0300

    feat(env): validate BASE_API_URL using yup schema

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,11 @@
-const env = {
-  BASE_API_URL: import.meta.env.VITE_API_URL!,
-};
+import * as yup from 'yup';
+
+const envSchema = yup.object().shape({
+  BASE_API_URL: yup.string().url().required(),
+});
+
+const env = envSchema.cast({
+  BASE_API_URL: import.meta.env.VITE_API_URL,
+});
 
 export default env;
