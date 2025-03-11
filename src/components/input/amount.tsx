78fa46568f9ec5bf6d3ce4224a50d40b5ee3eea0
MateusDiniz9 <mateus.090998@gmail.com>
2025-03-11 17:55:46 -0300

    feat(transaction): prevent consumed tokens from being reused, add thousand separators in amount

--- src/components/input/amount.tsx
+++ src/components/input/amount.tsx
@@ -2,15 +2,16 @@ import { Input, InputProps } from '@chakra-ui/react';
 import MaskedInput from 'react-text-mask';
 import { createNumberMask } from 'text-mask-addons';
 
+// A máscara de número com separação de milhar e ponto decimal
 const currencyMask = createNumberMask({
   prefix: '',
   suffix: '',
   includeThousandsSeparator: true,
-  thousandsSeparatorSymbol: '',
+  thousandsSeparatorSymbol: ',', // Usando vírgula como separador de milhar
   allowDecimal: true,
   decimalSymbol: '.',
-  decimalLimit: 9, // how many digits allowed after the decimal
-  integerLimit: 9, // limit length of integer numbers
+  decimalLimit: 9, // Limita a 9 casas decimais
+  integerLimit: 9, // Limita o número de dígitos inteiros
   allowNegative: false,
   allowLeadingZeroes: false,
 });
@@ -25,49 +26,11 @@ const AmountInput = (props: AmountInputProps) => (
     value={props.value}
     onChange={(event) => {
       const inputValue = event.target.value;
-      const nativeEvent = event.nativeEvent as InputEvent;
-      const isComma = nativeEvent.data === ',';
 
-      // This if case just handle the comma (',') to replace it to a dot ('.')
-      if (isComma && !inputValue.endsWith('.')) {
-        const caretPosition = event.target.selectionStart ?? 0;
+      // Remove qualquer caractere não numérico, exceto ponto
+      event.target.value = inputValue.replace(/[^0-9.,]/g, '');
 
-        // Determine if the comma is the first character
-        const isFirstChar = inputValue.length === 0;
-        const complement = isFirstChar ? '0.' : '.';
-
-        // Build the new value by appending or replacing the comma with a dot
-        const newValue = isFirstChar
-          ? complement
-          : inputValue.slice(0, caretPosition) +
-            complement +
-            inputValue.slice(caretPosition);
-
-        // Update the input value with the new value containing the dot
-        event.target.value = newValue;
-
-        // Adjust the "focus" position after the dot is inserted
-        const newCaretPosition = caretPosition + (isFirstChar ? 2 : 1);
-        event.target.setSelectionRange(newCaretPosition, newCaretPosition);
-
-        props.onChange?.(event);
-        return;
-      }
-
-      const isBackspace = inputValue.length < (props.value as string).length;
-
-      if (
-        inputValue.startsWith('0') &&
-        inputValue.length === 1 &&
-        isBackspace
-      ) {
-        event.target.value = ``;
-      } else if (inputValue.startsWith('0') && inputValue.length === 1) {
-        event.target.value = `0.`;
-      }
-
-      event.target.value = event.target.value.replace(/[^0-9.]/g, '');
-
+      // Lógica para atualizar o valor no estado do componente
       props.onChange?.(event);
     }}
     render={(maskedInputRef, maskedInputProps) => (
