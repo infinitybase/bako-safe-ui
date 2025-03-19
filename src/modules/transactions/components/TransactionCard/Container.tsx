75ba74b0812af41c83a101cb7d736da76b34251e
vitor <pereiravitor1218@gmail.com>
2025-03-19 19:43:28 -0300

    refactor(transaction): add GroupMonth, Icon, and Header components to TransactionCard

--- src/modules/transactions/components/TransactionCard/Container.tsx
+++ src/modules/transactions/components/TransactionCard/Container.tsx
@@ -1,24 +1,17 @@
 import {
-  AccordionButton,
   AccordionItem,
   AccordionPanel,
   Box,
   CardProps,
-  Flex,
-  Grid,
-  HStack,
-  Icon,
   VStack,
 } from '@chakra-ui/react';
-import { ReactNode } from 'react';
+import { memo, ReactNode, useMemo } from 'react';
 
-import { Card, DownLeftArrowGreen, UpRightArrowYellow } from '@/components';
-import { ContractIcon } from '@/components/icons/tx-contract';
-import { DeployIcon } from '@/components/icons/tx-deploy';
+import { Card } from '@/components';
 import { TransactionState } from '@/modules/core';
 import { useWorkspaceContext } from '@/modules/workspace/WorkspaceProvider';
 
-import { TransactionCard, transactionStatus } from '../..';
+import { TransactionCard } from '../..';
 import { useDetailsDialog } from '../../hooks/details';
 import { useVerifyTransactionInformations } from '../../hooks/details/useVerifyTransactionInformations';
 import { TransactionWithVault } from '../../services/types';
@@ -34,155 +27,104 @@ interface TransactionCardContainerProps extends CardProps {
   callBack?: () => void;
 }
 
-const Container = ({
-  status,
-  details,
-  transaction,
-  account,
-  isSigner,
-  isInTheVaultPage,
-  callBack,
-  ...rest
-}: TransactionCardContainerProps) => {
-  const { isSigned, isCompleted, isDeclined, isReproved, isCanceled } = status;
+const Container = memo(
+  ({
+    status,
+    details,
+    transaction,
+    account,
+    isSigner,
+    isInTheVaultPage,
+    callBack,
+    ...rest
+  }: TransactionCardContainerProps) => {
+    const { isSigned, isCompleted, isDeclined, isReproved, isCanceled } =
+      status;
 
-  const {
-    screenSizes: { isMobile },
-  } = useWorkspaceContext();
+    const {
+      screenSizes: { isMobile },
+    } = useWorkspaceContext();
 
-  const missingSignature =
-    !isSigned && !isCanceled && !isCompleted && !isDeclined && !isReproved;
+    const missingSignature = useMemo(
+      () =>
+        !isSigned && !isCanceled && !isCompleted && !isDeclined && !isReproved,
+      [isSigned, isCanceled, isCompleted, isDeclined, isReproved],
+    );
 
-  const {
-    isFromConnector,
-    isFromCLI,
-    isDeploy,
-    isDeposit,
-    isContract,
-    isFuelFriday,
-    isMint,
-    showAmountInformations,
-  } = useVerifyTransactionInformations(transaction);
+    const {
+      isDeposit,
+      isContract,
+      isFuelFriday,
+      isMint,
+      showAmountInformations,
+    } = useVerifyTransactionInformations(transaction);
 
-  const detailsDialog = useDetailsDialog();
+    const { isOpen, onClose, onOpen } = useDetailsDialog();
 
-  return (
-    <>
-      {transaction && (
-        <DetailsDialog
-          isOpen={detailsDialog.isOpen}
-          onClose={detailsDialog.onClose}
-          transaction={transaction}
-          account={account}
-          status={status}
-          isSigner={isSigner}
-          isInTheVaultPage={isInTheVaultPage}
-          callBack={callBack}
-          isContract={isContract}
-        />
-      )}
-
-      <Card
-        pl={0}
-        pr={{ base: 2, sm: 4, md: isInTheVaultPage ? 4 : 0, lg: 4 }}
-        py={0}
-        w="full"
-        as={AccordionItem}
-        backdropFilter="blur(16px)"
-        borderColor={
-          missingSignature ? 'warning.500' : 'gradients.transaction-border'
-        }
-        bg="gradients.transaction-card"
-        boxShadow="0px 8px 6px 0px #00000026"
-        maxW="full"
-        {...rest}
-        type={isFuelFriday ? 'green-gradient' : 'default'}
-        display="flex"
-      >
-        <Flex
-          alignItems="flex-start"
-          justifyContent="center"
-          bgColor="grey.925"
-          minW="32px"
-          p={0}
-          borderRadius="10px 0 0 10px"
-          h="auto"
-        >
-          <Icon
-            as={
-              isDeploy
-                ? DeployIcon
-                : isFromConnector || isFromCLI
-                  ? ContractIcon
-                  : isDeposit
-                    ? DownLeftArrowGreen
-                    : UpRightArrowYellow
-            }
-            mt={8}
-            fontSize={isDeploy || isFromConnector ? 'inherit' : '12px'}
+    return (
+      <>
+        {transaction && isOpen && (
+          <DetailsDialog
+            isOpen={isOpen}
+            onClose={onClose}
+            transaction={transaction}
+            account={account}
+            status={status}
+            isSigner={isSigner}
+            isInTheVaultPage={isInTheVaultPage}
+            callBack={callBack}
+            isContract={isContract}
           />
-        </Flex>
-        <VStack
-          justifyContent="center"
-          gap={0}
+        )}
+
+        <Card
+          pl={0}
+          pr={{ base: 2, sm: 4, md: isInTheVaultPage ? 4 : 0, lg: 4 }}
+          py={0}
           w="full"
-          maxW={{ base: 890, lg: 'unset' }}
+          as={AccordionItem}
+          backdropFilter="blur(16px)"
+          borderColor={
+            missingSignature ? 'warning.500' : 'gradients.transaction-border'
+          }
+          bg="gradients.transaction-card"
+          boxShadow="0px 8px 6px 0px #00000026"
+          maxW="full"
+          {...rest}
+          type={isFuelFriday ? 'green-gradient' : 'default'}
+          display="flex"
         >
-          <HStack
-            as={isMobile ? Box : AccordionButton}
-            onClick={detailsDialog.onOpen}
+          <TransactionCard.Icon transaction={transaction} />
+          <VStack
+            justifyContent="center"
+            gap={0}
             w="full"
-            _hover={{ bgColor: 'transparent' }}
-            px={{ base: 2, sm: 4 }}
-            py={2}
+            maxW={{ base: 890, lg: 'unset' }}
           >
-            <Grid
-              w="full"
-              gap={{ base: 2, sm: 4 }}
-              templateColumns="repeat(4, 1fr)"
-            >
-              {transaction.predicate && (
-                <TransactionCard.BasicInfos
-                  h={'59px'}
-                  justifyContent={'center'}
-                  vault={transaction.predicate}
-                  transactionName={
-                    isFuelFriday ? 'Fuel Friday' : transaction.name
-                  }
-                />
-              )}
+            <TransactionCard.Header
+              isMobile={isMobile}
+              onOpenDialog={onOpen}
+              status={status}
+              transaction={transaction}
+              isDeposit={isDeposit}
+              isFuelFriday={isFuelFriday}
+              isMint={isMint}
+              isSigner={isSigner}
+              showAmountInformations={showAmountInformations}
+            />
 
-              <TransactionCard.Amount
-                transaction={transaction}
-                showAmount={!showAmountInformations || isDeposit || isMint}
-              />
-              <TransactionCard.Status
-                transaction={transaction}
-                status={transactionStatus({
-                  ...transaction,
-                  account,
-                })}
-              />
-              <TransactionCard.Actions
-                transaction={transaction}
-                isSigner={isSigner}
-                status={transactionStatus({
-                  ...transaction,
-                  account,
-                })}
-              />
-            </Grid>
-          </HStack>
+            <Box w="full">
+              <AccordionPanel px={{ base: 2, sm: 4 }} w="full">
+                {details}
+              </AccordionPanel>
+            </Box>
+          </VStack>
+        </Card>
+      </>
+    );
+  },
+);
 
-          <Box w="full">
-            <AccordionPanel px={{ base: 2, sm: 4 }} w="full">
-              {details}
-            </AccordionPanel>
-          </Box>
-        </VStack>
-      </Card>
-    </>
-  );
-};
+Container.displayName = 'TransactionCardContainer';
 
 export { Container };
