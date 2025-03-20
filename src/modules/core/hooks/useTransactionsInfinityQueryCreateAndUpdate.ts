e6127ba00ca7f8bb5800abea82fcd3f8b6f14cc5
Erik William Tahara Gonçalves <erikwtg@gmail.com>
2025-03-20 17:13:01 -0300

    chore(transaction): fixed real-time infinity query update

--- src/modules/core/hooks/useTransactionsInfinityQueryCreateAndUpdate.ts
+++ src/modules/core/hooks/useTransactionsInfinityQueryCreateAndUpdate.ts
@@ -5,18 +5,28 @@ export const useTransactionsInfinityQueryCreateAndUpdate = (
   if (!oldData) return oldData;
 
   const { type, transaction } = newTransaction;
+  const { pageParams, pages } = oldData;
 
   if (type === '[UPDATED]') {
     return {
-      ...oldData,
-      pages: oldData.pages.map((page: any) =>
-        page.id === transaction.id ? transaction : page,
+      pageParams,
+      pages: pages.map((page: any) =>
+        page.data.some((item: any) => item.id === transaction.id)
+          ? {
+              ...page,
+              data: page.data.map((item: any) =>
+                item.id === transaction.id ? transaction : item,
+              ),
+            }
+          : page,
       ),
     };
   }
 
   return {
-    ...oldData,
-    pages: [transaction, ...oldData.pages[0].data],
+    pageParams,
+    pages: pages.map((page: any, index: number) =>
+      index === 0 ? { ...page, data: [transaction, ...page.data] } : page,
+    ),
   };
 };
