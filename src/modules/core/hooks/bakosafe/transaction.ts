70a3e92f4c706d75f730fd29ae67bd8f6302138a
MateusDiniz9 <mateus.090998@gmail.com>
2025-03-31 16:40:38 -0300

    fix(transaction): fix , error on create transaction

--- src/modules/core/hooks/bakosafe/transaction.ts
+++ src/modules/core/hooks/bakosafe/transaction.ts
@@ -51,30 +51,23 @@ const useBakoSafeCreateTransaction = ({
   return useBakoSafeMutation(
     TRANSACTION_QUERY_KEYS.DEFAULT,
     async (payload: IPayloadTransfer) => {
-      try {
-        const { hashTxId } = await vault.transaction({
-          name: payload.name!,
-          assets: payload.assets.map((asset) => {
-            const { units } = getAssetInfo(assetsMap, asset.assetId);
+      const { hashTxId } = await vault.transaction({
+        name: payload.name!,
+        assets: payload.assets.map((asset) => {
+          const { units } = getAssetInfo(assetsMap, asset.assetId);
 
-            return {
-              ...asset,
-              amount: bn
-                .parseUnits(asset.amount.replace(/,/g, ''), units)
-                .format(),
-            };
-          }),
-        });
-
-        const transaction = await TransactionService.getByHash(hashTxId, [
-          TransactionStatus.AWAIT_REQUIREMENTS,
-        ]);
-
-        return transaction;
-      } catch (error) {
-        console.error('Erro ao criar transação:', error); // Logando o erro
-        throw error; // Re-throw do erro para que o hook de mutação possa lidar com ele
-      }
+          return {
+            ...asset,
+            amount: bn
+              .parseUnits(asset.amount.replace(/,/g, ''), units)
+              .format(),
+          };
+        }),
+      });
+      const transaction = await TransactionService.getByHash(hashTxId, [
+        TransactionStatus.AWAIT_REQUIREMENTS,
+      ]);
+      return transaction;
     },
     options,
   );
