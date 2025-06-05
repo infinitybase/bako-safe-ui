997c0b652a25dfaec4dea8d199b1bff68beaebab
vitor <pereiravitor1218@gmail.com>
2025-06-05 17:24:22 -0300

    feat(AssetsIcon): integrate asset metadata fetching and improve loading state handling

--- src/modules/transactions/components/TransactionCard/AssetsIcon.tsx
+++ src/modules/transactions/components/TransactionCard/AssetsIcon.tsx
@@ -1,8 +1,11 @@
-import { AvatarGroup, Image } from '@chakra-ui/react';
+import { AvatarGroup, Image, Skeleton } from '@chakra-ui/react';
 import { ITransferAsset } from 'bakosafe';
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 
 import { AssetMap } from '@/modules/core';
+import { parseURI } from '@/modules/core/utils/formatter';
+
+import { useGetAssetsMetadata } from '../../hooks/assets/useGetAssetsMetadata';
 interface AssetsIconProps {
   assets: ITransferAsset[];
   isMobile: boolean;
@@ -13,6 +16,23 @@ interface AssetsIconProps {
 
 export const AssetsIcon = memo(
   ({ assets, isMobile, showOnlyOneAsset, assetsMap }: AssetsIconProps) => {
+    const { assets: metadataAssets, isLoading } = useGetAssetsMetadata(
+      assets.map((asset) => asset.assetId),
+    );
+
+    const assetsWithImage = useMemo(
+      () =>
+        assets.map((asset) => ({
+          ...asset,
+          image: parseURI(
+            metadataAssets?.[asset.assetId]?.metadata?.image ||
+              metadataAssets?.[asset.assetId]?.icon ||
+              assetsMap.UNKNOWN.icon!,
+          ),
+        })),
+      [assets, metadataAssets, assetsMap],
+    );
+
     return (
       <AvatarGroup
         max={showOnlyOneAsset ? 1 : 2}
@@ -20,16 +40,23 @@ export const AssetsIcon = memo(
         justifyContent={isMobile ? 'start' : 'end'}
         position="relative"
       >
-        {assets.map((asset) => (
-          <Image
+        {assetsWithImage.map((asset) => (
+          <Skeleton
             key={asset.assetId}
+            isLoaded={!isLoading}
+            borderRadius="full"
             w={{ base: '30.5px', sm: 6 }}
             h={{ base: 'full', sm: 6 }}
-            src={assetsMap[asset.assetId]?.icon ?? assetsMap.UNKNOWN.icon}
-            borderRadius={100}
-            alt="Asset Icon"
-            objectFit="cover"
-          />
+          >
+            <Image
+              w={{ base: '30.5px', sm: 6 }}
+              h={{ base: 'full', sm: 6 }}
+              src={asset.image}
+              borderRadius={100}
+              alt="Asset Icon"
+              objectFit="cover"
+            />
+          </Skeleton>
         ))}
       </AvatarGroup>
     );
