6b32eeb6daf0419009b8a29a7af6abc035de3746
Rafael <rafael-dev@hotmail.com>
2025-07-16 22:03:31 -0300

    fix: center nft image skeleton spinner and add a default metadata to nfts

--- src/modules/core/components/assetBalance/nft-image.tsx
+++ src/modules/core/components/assetBalance/nft-image.tsx
@@ -1,4 +1,4 @@
-import { Center, Image, Spinner } from '@chakra-ui/react';
+import { Box, Center, Image, Spinner } from '@chakra-ui/react';
 import { useEffect, useRef, useState } from 'react';
 
 import { CustomSkeleton } from '@/components';
@@ -68,16 +68,12 @@ export const NftImage = ({
 
   if (state.isLoading) {
     return (
-      <>
+      <Box w="full" h="full" position="relative">
         <CustomSkeleton
           startColor="dark.200"
           endColor="dark.500"
           w="full"
           h="full"
-          position="absolute"
-          top={0}
-          left={0}
-          zIndex={0}
         />
         <Center
           w="full"
@@ -89,7 +85,7 @@ export const NftImage = ({
         >
           <Spinner thickness="3px" speed="0.5s" color="grey.400" size="md" />
         </Center>
-      </>
+      </Box>
     );
   }
 
