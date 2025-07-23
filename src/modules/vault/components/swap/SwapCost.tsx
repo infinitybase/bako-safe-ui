3324ee1b6222afcbbbde0970dbfe4e741f433d9f
vitor <pereiravitor1218@gmail.com>
2025-07-23 13:08:37 -0300

    feat(swap): add SwapCost and SwapDivider components for displaying swap rates and routing

---
+++ src/modules/vault/components/swap/SwapCost.tsx
@@ -0,0 +1,72 @@
+import { Card, Flex, Spinner, Stack, Text } from '@chakra-ui/react';
+import { TransactionCost } from 'fuels';
+import { PoolId } from 'mira-dex-ts';
+import { memo } from 'react';
+
+import { useSwapRate } from '../../hooks/swap/useSwapRate';
+import { SwapState } from './Root';
+import { SwapRouteItem } from './SwapRouteItem';
+
+interface SwapCostProps {
+  txCost?: TransactionCost;
+  isLoadingCost: boolean;
+  swapState: SwapState;
+  pools: PoolId[];
+}
+
+export const SwapCost = memo(function SwapCost({
+  txCost,
+  isLoadingCost,
+  swapState,
+  pools,
+}: SwapCostProps) {
+  const rate = useSwapRate({ swapState });
+
+  return (
+    <Card variant="outline" p={3} mt={1}>
+      <Stack spacing={1}>
+        <Flex alignItems="center" justifyContent="space-between">
+          <Text color="section.500" fontSize="xs">
+            Rate:
+          </Text>
+          <Text color="grey.75" fontSize="xs">
+            {rate}
+          </Text>
+        </Flex>
+
+        <Flex alignItems="center" justifyContent="space-between">
+          <Text color="section.500" fontSize="xs">
+            Routing:
+          </Text>
+          {pools.map((pool) => (
+            <SwapRouteItem key={String(pool)} pool={pool} />
+          ))}
+        </Flex>
+
+        <Flex alignItems="center" justifyContent="space-between">
+          <Text color="section.500" fontSize="xs">
+            Estimated fees:
+          </Text>
+          <Text color="grey.75" fontSize="xs">
+            {swapState.fee?.formatUnits(9)} {'ETH'}
+          </Text>
+        </Flex>
+
+        <Flex alignItems="center" justifyContent="space-between">
+          {isLoadingCost && <Spinner size="xs" ml="auto" color="section.500" />}
+
+          {!isLoadingCost && txCost?.gasPrice && (
+            <>
+              <Text color="section.500" fontSize="xs">
+                Gas cost:
+              </Text>
+              <Text color="grey.75" fontSize="xs">
+                {txCost?.gasPrice.formatUnits(9)} {'ETH'}
+              </Text>
+            </>
+          )}
+        </Flex>
+      </Stack>
+    </Card>
+  );
+});
