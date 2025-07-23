38a13c04f24de8914b69464b5a1b34557790697b
vitor <pereiravitor1218@gmail.com>
2025-07-23 13:08:51 -0300

    feat(swap): add swap functionality to sidebar and implement VaultSwapPage

---
+++ src/modules/vault/pages/swap/index.tsx
@@ -0,0 +1,144 @@
+import {
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Container,
+  Divider,
+  Flex,
+  HStack,
+  Icon,
+  Skeleton,
+  Stack,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { RiMenuUnfoldLine } from 'react-icons/ri';
+import { useNavigate } from 'react-router-dom';
+
+import { HomeIcon } from '@/components';
+import { Drawer } from '@/layouts/dashboard/drawer';
+import { Pages, useBakoSafeVault } from '@/modules/core';
+import { useWorkspaceContext } from '@/modules/workspace/WorkspaceProvider';
+
+import { RootSwap } from '../../components/swap/Root';
+import { useAssetsList } from '../../hooks';
+import { useVaultInfosContext } from '../../VaultInfosProvider';
+
+export const VaultSwapPage = () => {
+  const navigate = useNavigate();
+  const menuDrawer = useDisclosure();
+  const { vault } = useVaultInfosContext();
+  const { data: bakoVault, isLoading: isLoadingVault } = useBakoSafeVault({
+    id: vault?.data?.id,
+    address: vault?.data?.predicateAddress,
+  });
+  const { assets, isLoading: isLoadingAssets } = useAssetsList({
+    vault: bakoVault,
+  });
+
+  const {
+    authDetails: { userInfos },
+    workspaceInfos: {
+      handlers: { goHome },
+    },
+    screenSizes: { vaultRequiredSizeToColumnLayout },
+  } = useWorkspaceContext();
+
+  const workspaceId = userInfos.workspace?.id ?? '';
+
+  const isLoading = isLoadingAssets || isLoadingVault || vault.isLoading;
+
+  return (
+    <Box w="full">
+      <Drawer isOpen={menuDrawer.isOpen} onClose={menuDrawer.onClose} />
+
+      <HStack mb={8} w="full" justifyContent="space-between">
+        {vaultRequiredSizeToColumnLayout ? (
+          <HStack gap={1.5} onClick={menuDrawer.onOpen}>
+            <Icon as={RiMenuUnfoldLine} fontSize="xl" color="grey.200" />
+            <Text fontSize="sm" fontWeight="normal" color="grey.100">
+              Menu
+            </Text>
+          </HStack>
+        ) : (
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <BreadcrumbLink
+                fontSize="sm"
+                color="grey.200"
+                fontWeight="semibold"
+                onClick={() => goHome()}
+              >
+                <Icon mr={2} as={HomeIcon} fontSize="sm" color="grey.200" />
+                Home
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbItem>
+              <BreadcrumbLink
+                fontSize="sm"
+                color="grey.200"
+                fontWeight="semibold"
+                href="#"
+                onClick={() =>
+                  navigate(
+                    Pages.userVaults({
+                      workspaceId,
+                    }),
+                  )
+                }
+              >
+                Vaults
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <BreadcrumbLink
+                fontSize="sm"
+                color="grey.200"
+                fontWeight="semibold"
+                onClick={() =>
+                  navigate(
+                    Pages.detailsVault({
+                      vaultId: vault.data?.id,
+                      workspaceId: userInfos.workspace?.id ?? '',
+                    }),
+                  )
+                }
+                isTruncated
+                maxW={640}
+              >
+                {vault?.data?.name}
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <BreadcrumbLink
+                fontSize="sm"
+                color="grey.200"
+                fontWeight="semibold"
+                href="#"
+              >
+                Swap
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>
+        )}
+      </HStack>
+
+      <Container>
+        {isLoading && (
+          <Stack spacing={4}>
+            <Skeleton height="150px" />
+            <Divider />
+            <Skeleton height="150px" />
+            <Flex gap={4}>
+              <Skeleton w="130px" height="40px" />
+              <Skeleton w="full" height="40px" />
+            </Flex>
+          </Stack>
+        )}
+        {!isLoading && <RootSwap vault={bakoVault} assets={assets} />}
+      </Container>
+    </Box>
+  );
+};
