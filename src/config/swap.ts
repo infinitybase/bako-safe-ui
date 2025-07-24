5b8d90be62b207be183d721e6671ee864d1f2b03
vitor <pereiravitor1218@gmail.com>
2025-07-24 11:18:12 -0300

    feat(swap): update Bako fee and address to use environment variables

--- src/config/swap.ts
+++ src/config/swap.ts
@@ -1,5 +1,26 @@
+const FEE = import.meta.env.VITE_BAKO_SWAP_FEE;
+const ADDRESS = import.meta.env.VITE_BAKO_SWAP_ADDRESS;
+
+if (!FEE) {
+  throw new Error(
+    'VITE_BAKO_SWAP_FEE is not defined in the environment variables',
+  );
+}
+
+if (!ADDRESS) {
+  throw new Error(
+    'VITE_BAKO_SWAP_ADDRESS is not defined in the environment variables',
+  );
+}
+
 /**
  * Bako fee percentage for swaps.
  * This is a fixed fee applied to all swaps.
  */
-export const BAKO_FEE_PERCENTAGE = 0.03; // 3% fee
+export const BAKO_FEE_PERCENTAGE = parseInt(FEE);
+
+/**
+ * Bako fee address for swaps.
+ * This is the address where the fees are sent.
+ */
+export const BAKO_FEE_ADDRESS: string = ADDRESS;
