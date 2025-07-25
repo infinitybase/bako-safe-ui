e37fa566dd0f7b087d1d518e88423c9574118fd6
vitor <pereiravitor1218@gmail.com>
2025-07-25 18:03:34 -0300

    feat(swap): integrate transaction refetching on successful swap execution

--- src/modules/vault/hooks/swap/useSwap.ts
+++ src/modules/vault/hooks/swap/useSwap.ts
@@ -2,7 +2,20 @@ import { useMutation } from '@tanstack/react-query';
 import { Vault } from 'bakosafe';
 import { TransactionRequestLike } from 'fuels';
 
+import { useTransactionsContext } from '@/modules/transactions/providers/TransactionsProvider';
+
 export const useSwap = () => {
+  const {
+    transactionsPageList: {
+      request: { refetch: refetchTransactionsList },
+    },
+    homeTransactions: {
+      request: { refetch: refetchHomeTransactionsList },
+    },
+    vaultTransactions: {
+      request: { refetch: refetchVaultTransactionsList },
+    },
+  } = useTransactionsContext();
   const { mutateAsync: sendTx, ...rest } = useMutation({
     mutationKey: ['swap'],
     mutationFn: ({
@@ -20,6 +33,11 @@ export const useSwap = () => {
         throw new Error('Vault is not available');
       }
       return vault.BakoTransfer(tx, { name: `Swap ${assetIn} to ${assetOut}` });
+    },
+    onSuccess: () => {
+      refetchTransactionsList();
+      refetchHomeTransactionsList();
+      refetchVaultTransactionsList();
     },
   });
 
