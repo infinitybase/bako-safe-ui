c81753c262842896f48b6f03a9920915cc514fdf
vitor <pereiravitor1218@gmail.com>
2025-08-06 11:46:14 -0300

    fix(swap): remove unused swap address environment variable and related checks

--- src/config/swap.ts
+++ src/config/swap.ts
@@ -1,17 +1,10 @@
 import { MiraIcon } from '@/components';
 
 const FEE = import.meta.env.VITE_BAKO_SWAP_FEE;
-const ADDRESS = import.meta.env.VITE_BAKO_SWAP_ADDRESS;
 
 if (!FEE) {
   throw new Error(
     'VITE_BAKO_SWAP_FEE is not defined in the environment variables',
-  );
-}
-
-if (!ADDRESS) {
-  throw new Error(
-    'VITE_BAKO_SWAP_ADDRESS is not defined in the environment variables',
   );
 }
 
@@ -20,12 +13,6 @@ if (!ADDRESS) {
  * This is a fixed fee applied to all swaps.
  */
 export const BAKO_FEE_PERCENTAGE = parseInt(FEE);
-
-/**
- * Bako fee address for swaps.
- * This is the address where the fees are sent.
- */
-export const BAKO_FEE_ADDRESS: string = ADDRESS;
 
 export const DEFAULT_SLIPPAGE = 0.01; // 1%
 
