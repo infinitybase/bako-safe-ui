5e5c50574cb93fd42139f923abbaa2befe4fcf7e
vitor <pereiravitor1218@gmail.com>
2025-08-07 22:14:12 -0300

    fix(swap): remove trailing slash from microchain origin URL

--- src/config/swap.ts
+++ src/config/swap.ts
@@ -24,7 +24,7 @@ export enum SwapButtonTitle {
 
 export const miraData = {
   name: 'Mira on Microchain',
-  origin: 'https://microchain.systems/',
+  origin: 'https://microchain.systems',
   icon: MiraIcon,
 };
 
