9ac6a4360ed36ee4b7c85832d12db9223c2f3eeb
vitor <pereiravitor1218@gmail.com>
2025-08-13 11:17:51 -0300

    fix(useAPY): add proxy only in dev

--- src/modules/vault/hooks/liquid-stake/useAPY.ts
+++ src/modules/vault/hooks/liquid-stake/useAPY.ts
@@ -16,6 +16,10 @@ interface Inflation {
 
 const TOTAL_FUEL_SUPPLY = 10000000000000000000;
 
+const BASE_SEQUENCER_API = import.meta.env.DEV
+  ? '/rig'
+  : import.meta.env.VITE_SEQUENCER_URL;
+
 export const useAPY = () => {
   const {
     data: data,
@@ -25,11 +29,11 @@ export const useAPY = () => {
     queryKey: ['rig-apy'],
     queryFn: async () => {
       const inflationResponse = await request<Inflation>(
-        '/rig/cosmos/mint/v1beta1/inflation',
+        BASE_SEQUENCER_API + '/cosmos/mint/v1beta1/inflation',
       );
 
       const poolResponse = await request<StakingPool>(
-        '/rig/cosmos/staking/v1beta1/pool',
+        BASE_SEQUENCER_API + '/cosmos/staking/v1beta1/pool',
       );
 
       if (!inflationResponse) throw new Error('Error on get inflation');
