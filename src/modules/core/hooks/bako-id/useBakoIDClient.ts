bb20072f7fa77b7526f7dfaa734d833d9b14e7aa
vitor <pereiravitor1218@gmail.com>
2025-08-14 15:49:43 -0300

    feat(avatar): add useAvatar hook to fetch user avatar based on name and chain ID

--- src/modules/core/hooks/bako-id/useBakoIDClient.ts
+++ src/modules/core/hooks/bako-id/useBakoIDClient.ts
@@ -10,6 +10,9 @@ import { BakoProvider } from 'bakosafe';
 import { Provider } from 'fuels';
 import { useCallback } from 'react';
 
+import { localStorageKeys } from '@/modules/auth';
+import { availableNetWorks, NetworkType } from '@/modules/network/services';
+
 const client = new BakoIDClient();
 
 const BAKOID_QUERY_KEYS = {
@@ -24,6 +27,10 @@ const BAKOID_QUERY_KEYS = {
   },
   names: (addresses: string[]) => {
     const base = BAKOID_QUERY_KEYS.base.concat(['names', ...addresses]);
+    return base as QueryKey;
+  },
+  avatar: (name: string) => {
+    const base = BAKOID_QUERY_KEYS.base.concat(['avatar', name]);
     return base as QueryKey;
   },
 };
@@ -167,4 +174,18 @@ export const useBakoIDClient = (providerInstance: Promise<BakoProvider>) => {
       },
     },
   };
+};
+
+export const useAvatar = (name: string) => {
+  const chainId = Number(
+    localStorage.getItem(localStorageKeys.SELECTED_CHAIN_ID) ??
+      availableNetWorks[NetworkType.MAINNET].chainId,
+  );
+  const { data: avatar, ...rest } = useQuery({
+    queryFn: () => {
+      return client.avatar(name, chainId);
+    },
+  });
+
+  return { avatar, ...rest };
 };
