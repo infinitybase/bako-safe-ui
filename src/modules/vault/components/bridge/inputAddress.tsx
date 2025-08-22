1095f156e6be0c904989e7597290b2232a056429
caiorocha <caio.cesar209@gmail.com>
2025-08-22 15:07:39 -0300

    feat(components): create components to feed bridge pages

---
+++ src/modules/vault/components/bridge/inputAddress.tsx
@@ -0,0 +1,85 @@
+import {
+  Card,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
+
+import { LineCloseIcon } from '@/components';
+
+import { ITransferBridgePayload } from '../../pages';
+
+export function InputAddressBridge() {
+  const { control } = useFormContext<ITransferBridgePayload>();
+  const [isFocused, setIsFocused] = useState(false);
+
+  return (
+    <Card variant="outline" padding={3}>
+      <VStack p={0} gap={1} align={'flex-start'}>
+        <Text color="#868079" fontSize={12} flex={1}>
+          Destination address
+        </Text>
+        <Controller
+          control={control}
+          name="destinationAddress"
+          render={({ field, fieldState }) => (
+            <FormControl isInvalid={fieldState.invalid} paddingTop={0}>
+              <InputGroup
+                sx={{
+                  paddingTop: 0,
+                  paddingBottom: 0,
+                  '> input': {
+                    paddingTop: 0,
+                    paddingBottom: 0,
+                    height: '46px',
+                  },
+                }}
+              >
+                <InputRightElement pr={3} top="35%" cursor={'pointer'}>
+                  {field.value && (
+                    <LineCloseIcon
+                      color="grey.75"
+                      fontSize={'16px'}
+                      onClick={() => field.onChange('')}
+                    />
+                  )}
+                </InputRightElement>
+
+                <Input
+                  maxLength={27}
+                  value={field.value?.trimStart()}
+                  onChange={field.onChange}
+                  placeholder=" "
+                  variant="dark"
+                  onFocus={() => setIsFocused(true)}
+                  onBlur={() => setIsFocused(false)}
+                  paddingTop={0}
+                />
+                {!isFocused && !field.value && (
+                  <FormLabel
+                    id="destination_addr"
+                    fontSize={'14px !important'}
+                    color={'#AAA6A1 !important'}
+                    paddingTop={0}
+                  >
+                    {'Enter address'}
+                  </FormLabel>
+                )}
+              </InputGroup>
+              <FormHelperText paddingTop={0} color="error.500">
+                {fieldState.error?.message}
+              </FormHelperText>
+            </FormControl>
+          )}
+        />
+      </VStack>
+    </Card>
+  );
+}
