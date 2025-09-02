10605f3d2893ca8392d20596849847990c1062dd
vitor <pereiravitor1218@gmail.com>
2025-09-02 14:14:38 -0300

    feat: add AssetIdWithCopy component for displaying and copying asset IDs

---
+++ src/components/address/assetIdWithCopy.tsx
@@ -0,0 +1,30 @@
+import { Flex } from '@chakra-ui/react';
+
+import { CopyAddressButton } from '../copyAddressButton';
+import { Address } from './basic';
+
+interface AssetIdWithCopy {
+  assetId: string;
+}
+
+export const AssetIdWithCopy = ({ assetId }: AssetIdWithCopy) => {
+  return (
+    <Flex
+      minW={{ base: '105px' }}
+      ml="auto"
+      w="full"
+      overflow="hidden"
+      alignItems="center"
+      gap={3}
+    >
+      <Address value={assetId} />
+
+      <CopyAddressButton
+        aria-label="Copy Asset Id"
+        size="xs"
+        minW={2}
+        addressToCopy={assetId}
+      />
+    </Flex>
+  );
+};
