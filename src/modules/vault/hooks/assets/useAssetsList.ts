25988eb591490645c95ba77b9fdfdaf1228c37ff
caiorocha <caio.cesar209@gmail.com>
2025-09-04 16:09:16 -0300

    chore:(order assets): remove old validation

--- src/modules/vault/hooks/assets/useAssetsList.ts
+++ src/modules/vault/hooks/assets/useAssetsList.ts
@@ -62,10 +62,6 @@ export const useAssetsList = ({ vault }: { vault?: Vault }) => {
         })
         .concat(...noVerifiedAssets)
         .sort((a, b) => {
-          if (a.rate < 1 && b.rate < 1) {
-            return bn(a.balance).gt(bn(b.balance)) ? -1 : 1;
-          }
-
           if (a.balance && a.rate && b.balance && b.rate) {
             const aUsd = bn(a.balance)
               .mul(bn(Math.floor(a.rate * 10)))
@@ -73,6 +69,15 @@ export const useAssetsList = ({ vault }: { vault?: Vault }) => {
             const bUsd = bn(b.balance)
               .mul(bn(Math.floor(b.rate * 10)))
               .div(bn(10).pow(b.units));
+
+            console.log('>>>> names', a.name, b.name);
+            console.log(
+              '>>> BALANCES',
+              a.balance.toString(),
+              b.balance.toString(),
+            );
+            console.log('>>> RATES', a.rate, b.rate);
+            console.log('usds', aUsd.toString(), bUsd.toString());
 
             return aUsd.gt(bUsd) ? -1 : 1;
           }
