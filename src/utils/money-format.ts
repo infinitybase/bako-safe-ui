a677e0f52a683db9d7bae7cd8dfea1943777d20a
vitor <pereiravitor1218@gmail.com>
2025-09-16 16:19:37 -0300

    fix: update currency formatting logic for consistency across components

--- src/utils/money-format.ts
+++ src/utils/money-format.ts
@@ -1,8 +1,14 @@
 export const moneyFormat = (value: string, currency = 'USD'): string => {
+  const replacedValue =
+    currency === 'BRL' ? replaceBRLThousandSeparator(value) : value;
   return Intl.NumberFormat('en-US', {
     style: 'currency',
     currency,
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
-  }).format(Number(value.replace(/,/g, '')));
+  }).format(Number(replacedValue));
+};
+
+export const replaceBRLThousandSeparator = (value: string): string => {
+  return value.replace(/\./g, '').replace(/,/g, '.');
 };
