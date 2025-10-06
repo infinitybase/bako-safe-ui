ebedad69b72754c6165f392edfa1bc72193000bc
vitor <pereiravitor1218@gmail.com>
2025-10-06 10:51:25 -0300

    feat: add EditIcon2 component and export it in the icons index

---
+++ src/components/icons/edit-icon-2.tsx
@@ -0,0 +1,27 @@
+import { createIcon } from '@chakra-ui/icons';
+
+export const EditIcon2 = createIcon({
+  displayName: 'EditIcon2',
+  viewBox: '0 0 24 24',
+  path: (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#a)">
+        <path
+          d="M6.414 16 16.556 5.858l-1.414-1.414L5 14.586V16zm.83 2H3v-4.242L14.436 2.32a1 1 0 0 1 1.414 0l2.828 2.83a1 1 0 0 1 0 1.414zM3 20h18v2H3z"
+          fill="#AAA6A1"
+        />
+      </g>
+      <defs>
+        <clipPath id="a">
+          <path fill="#fff" d="M0 0h24v24H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  ),
+});
