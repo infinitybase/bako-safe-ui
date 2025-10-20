e272099a19d9dc7c0040757033259bf21fc6cd5c
vitor <pereiravitor1218@gmail.com>
2025-10-20 20:32:38 -0300

    refactor(VaultDetailsPage): simplify component structure by removing unused imports and enhancing layout with Grid

--- src/modules/vault/pages/details/index.tsx
+++ src/modules/vault/pages/details/index.tsx
@@ -1,38 +1,21 @@
 import { useQueryClient } from '@tanstack/react-query';
-import {
-  Box,
-  Breadcrumb,
-  Button,
-  HStack,
-  Icon,
-  Loader,
-  Spacer,
-  Text,
-} from 'bako-ui';
+import { Box, Grid, GridItem, Loader, Spacer, Text } from 'bako-ui';
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 
-import { CustomSkeleton, HomeIcon, TransactionTypeFilters } from '@/components';
+import { CustomSkeleton, TransactionFilters } from '@/components';
 import AddAssetsDialog from '@/components/addAssetsDialog';
 import DepositDialog from '@/components/depositDialog';
 import { EmptyState } from '@/components/emptyState';
-import { MenuIcon } from '@/components/icons/menu';
 import WelcomeDialog from '@/components/welcomeDialog';
-import { Drawer } from '@/layouts/dashboard/drawer';
 import { CardLiquidStake } from '@/modules';
-import { PermissionRoles } from '@/modules/core';
-import { useBakoSafeVault, useGetParams } from '@/modules/core/hooks';
+import { useBakoSafeVault } from '@/modules/core/hooks';
 import { useDisclosure } from '@/modules/core/hooks/useDisclosure';
-import { Pages } from '@/modules/core/routes';
-import { useTemplateStore } from '@/modules/template/store/useTemplateStore';
 import { TransactionCard, WaitingSignatureBadge } from '@/modules/transactions';
 import { useTransactionSocketListener } from '@/modules/transactions/hooks/events/useTransactionsSocketListener';
 import { useTransactionsContext } from '@/modules/transactions/providers/TransactionsProvider';
 import { useWorkspaceContext } from '@/modules/workspace/hooks';
-import { limitCharacters } from '@/utils/limit-characters';
 
-import { CardDetails } from '../../components/CardDetails';
-import { SignersDetails } from '../../components/SignersDetails';
+import { AccountAllocation, AccountOverview } from '../../components';
 import { useVaultInfosContext } from '../../hooks';
 import { vaultInfinityQueryKey } from '../../hooks/list/useVaultTransactionsRequest';
 
@@ -42,24 +25,26 @@ const VaultDetailsPage = () => {
   const {
     isOpen: addAssetsDialogState,
     onOpenChange: setAddAssetsDialogState,
-    setOpen: setIsAddAssetDialogOpen,
+    onOpen: setIsAddAssetDialogOpen,
   } = useDisclosure();
   const {
     isOpen: depositDialogState,
     onOpenChange: setDepositDialogState,
     setOpen: setOpenDepositDialog,
   } = useDisclosure();
-  const menuDrawer = useDisclosure();
-  const navigate = useNavigate();
-  const { vaultPageParams } = useGetParams();
   const { vault, assets } = useVaultInfosContext();
 
   const {
     vaultTransactions: {
-      filter: { txFilterType },
+      filter: { txFilterType, value: status },
       lists: { transactions },
       request: { isLoading, isFetching, queryKey },
-      handlers: { handleIncomingAction, handleOutgoingAction },
+      handlers: {
+        handleIncomingAction,
+        handleOutgoingAction,
+        handleAllAction,
+        handlePendingStatusChange,
+      },
       transactionsRef,
     },
     pendingSignerTransactions,
@@ -67,38 +52,18 @@ const VaultDetailsPage = () => {
   } = useTransactionsContext();
   const queryClient = useQueryClient();
 
-  const { setTemplateFormInitial } = useTemplateStore();
-
   const {
     authDetails: { userInfos },
-    workspaceInfos: {
-      handlers: {
-        // handleWorkspaceSelection,
-        hasPermission,
-        goHome,
-      },
-    },
-    screenSizes: {
-      vaultRequiredSizeToColumnLayout,
-      isSmall,
-      isMobile,
-      isLarge,
-    },
+    screenSizes: { isSmall, isMobile },
   } = useWorkspaceContext();
 
-  const workspaceId = userInfos.workspace?.id ?? '';
   const hasTransactions = !isLoading && transactions?.length;
 
-  const { OWNER, SIGNER } = PermissionRoles;
-
-  const canSetTemplate = hasPermission([SIGNER]) || hasPermission([OWNER]);
   const { vault: vaultSafe } = useBakoSafeVault({
     address: vault.data.predicateAddress,
     provider: vault.data.provider,
     id: vault.data.id,
   });
-
-  const hideSetTemplateButton = true;
 
   useEffect(() => {
     return () => {
@@ -111,14 +76,14 @@ const VaultDetailsPage = () => {
       vault.data?.id ?? undefined,
     );
 
+  const workspaceId = userInfos?.workspace?.id || '';
+
   useTransactionSocketListener(vaultQueryKey ?? []);
 
   if (!vault) return null;
 
   return (
     <Box w="full">
-      <Drawer open={menuDrawer.isOpen} onOpenChange={menuDrawer.onOpenChange} />
-
       <WelcomeDialog
         isOpen={welcomeDialogState}
         onOpenChange={setWelcomeDialogState}
@@ -137,150 +102,47 @@ const VaultDetailsPage = () => {
         setIsDepositDialogOpen={setOpenDepositDialog}
       />
 
-      <HStack mb={9} w="full" justifyContent="space-between">
-        {vaultRequiredSizeToColumnLayout ? (
-          <HStack gap={4} onClick={menuDrawer.onOpen}>
-            <Icon as={MenuIcon} w={6} color="grey.200" />
-            <Text fontSize="sm" fontWeight="normal" color="grey.100">
-              Menu
-            </Text>
-          </HStack>
-        ) : (
-          <Breadcrumb.Root>
-            <Breadcrumb.List>
-              <Breadcrumb.Item>
-                <Breadcrumb.Link
-                  fontSize="sm"
-                  color="grey.200"
-                  fontWeight="semibold"
-                  onClick={() => goHome()}
-                >
-                  <Icon mr={2} as={HomeIcon} w={3} color="grey.200" />
-                  Home
-                </Breadcrumb.Link>
-              </Breadcrumb.Item>
-              <Breadcrumb.Separator />
-
-              {/* Commented out code to temporarily disable workspaces. */}
-
-              {/* {!userInfos.onSingleWorkspace && (
-              <Breadcrumb.Item>
-                <Breadcrumb.Link
-                  fontSize="sm"
-                  color="grey.200"
-                  fontWeight="semibold"
-                  onClick={() =>
-                    handleWorkspaceSelection(
-                      workspaceId,
-                      Pages.workspace({
-                        workspaceId: userInfos.workspace?.id,
-                      }),
-                      true,
-                    )
-                  }
-                  maxW={40}
-                  isTruncated
-                >
-                  {userInfos.workspace?.name}
-                </Breadcrumb.Link>
-              </Breadcrumb.Item>
-            )} */}
-
-              <Breadcrumb.Item>
-                <Breadcrumb.Link
-                  fontSize="sm"
-                  color="grey.200"
-                  fontWeight="semibold"
-                  onClick={() =>
-                    navigate(
-                      Pages.userVaults({
-                        workspaceId,
-                      }),
-                    )
-                  }
-                >
-                  Vaults
-                </Breadcrumb.Link>
-              </Breadcrumb.Item>
-
-              <Breadcrumb.Separator />
-
-              <Breadcrumb.Item>
-                <Breadcrumb.Link
-                  fontSize="sm"
-                  color="grey.200"
-                  fontWeight="semibold"
-                  href="#"
-                  truncate
-                  maxW={640}
-                >
-                  {limitCharacters(vault?.data?.name ?? '', 25)}
-                </Breadcrumb.Link>
-              </Breadcrumb.Item>
-            </Breadcrumb.List>
-          </Breadcrumb.Root>
-        )}
-        {!hideSetTemplateButton && (
-          <Button
-            color="dark.200"
-            bgColor="grey.200"
-            fontWeight="medium"
-            fontSize={{ base: 'sm', sm: 'md' }}
-            border="none"
-            disabled={!canSetTemplate || true} // todo: fix this
-            onClick={() => {
-              if (
-                !vault.data?.id ||
-                !vault.data?.minSigners ||
-                !vault.data.members ||
-                !vaultPageParams.workspaceId
-              )
-                return;
-              setTemplateFormInitial({
-                minSigners: vault.data?.minSigners,
-                addresses:
-                  vault.data.members! &&
-                  vault.data?.members.map((signer) => signer.address),
-              });
-              navigate(
-                Pages.createTemplate({
-                  vaultId: vault.data.id!,
-                  workspaceId: vaultPageParams.workspaceId!,
-                }),
-              );
-            }}
-          >
-            Set as template
-          </Button>
-        )}
-      </HStack>
-
-      <CardLiquidStake assets={assets} vault={vaultSafe} />
-
-      <HStack
-        mb={{ base: 10, sm: 14 }}
-        alignItems="flex-start"
-        w="full"
+      <Grid
+        alignItems="stretch"
         gap={10}
+        templateColumns={{
+          base: 'repeat(1, 1fr)',
+          md: 'repeat(2, 1fr)',
+          lg: 'repeat(3, 1fr)',
+        }}
       >
-        <CardDetails
-          vault={vault}
-          assets={assets}
-          isPendingSigner={isPendingSigner}
-          setAddAssetsDialogState={setIsAddAssetDialogOpen}
-        />
+        <GridItem
+          colSpan={{
+            md: 2,
+            lg: 1,
+          }}
+        >
+          <AccountOverview
+            vault={vault}
+            workspaceId={workspaceId}
+            onAddAssets={setIsAddAssetDialogOpen}
+            isPendingSigner={isPendingSigner}
+          />
+        </GridItem>
+        <GridItem>
+          <AccountAllocation
+            assets={assets}
+            vault={vault}
+            workspaceId={workspaceId}
+          />
+        </GridItem>
+        <GridItem>
+          <CardLiquidStake assets={assets} vault={vaultSafe} />
+        </GridItem>
+      </Grid>
 
-        <SignersDetails
-          vault={vault}
-          display={{ base: 'none', sm: !isLarge ? 'block' : 'none' }}
-        />
-      </HStack>
       <Box
         w="full"
         display="flex"
         flexDir={{ base: 'column', sm: isSmall ? 'column' : 'row' }}
         gap={4}
-        mb={4}
+        mt={{ md: 10, base: 4 }}
+        mb={{ base: 4, md: 6 }}
       >
         <Box
           display="flex"
@@ -298,18 +160,21 @@ const VaultDetailsPage = () => {
         </Box>
         <Spacer />
 
-        <TransactionTypeFilters
+        <TransactionFilters
           currentFilter={txFilterType}
-          incomingAction={handleIncomingAction}
-          outgoingAction={handleOutgoingAction}
-          buttonsFullWidth={isSmall}
+          onIncomingFilter={handleIncomingAction}
+          onOutgoingFilter={handleOutgoingAction}
+          onAllFilter={handleAllAction}
+          status={status}
+          isPendingSignerTransaction={isPendingSigner}
+          onPendingFilter={handlePendingStatusChange}
         />
       </Box>
 
       <CustomSkeleton
         minH="30vh"
-        loading={vault.isLoading && isLoading}
-        h={!vault.isLoading && !isLoading ? 'unset' : '100px'}
+        loading={vault.isLoading || isLoading}
+        h={!vault.isLoading || !isLoading ? 'unset' : '100px'}
       >
         {hasTransactions
           ? transactions?.map((grouped, index) => {
@@ -319,7 +184,6 @@ const VaultDetailsPage = () => {
                   <TransactionCard.GroupMonth
                     monthYear={grouped.monthYear}
                     mb={!isMobile ? 3 : 0}
-                    mt={!isMobile ? 0 : 3}
                   />
                   <TransactionCard.List
                     w="full"
@@ -355,12 +219,6 @@ const VaultDetailsPage = () => {
               />
             )}
       </CustomSkeleton>
-
-      {isLarge && (
-        <Box mt={7}>
-          <SignersDetails vault={vault} />
-        </Box>
-      )}
     </Box>
   );
 };
