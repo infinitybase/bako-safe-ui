1be17d7dbc2a0fb2801460c91d527f1715fb7028
vitor <pereiravitor1218@gmail.com>
2025-10-27 15:55:30 -0300

    refactor(SwapButtonTitle): remove unused PREVIEW title for clarity

--- src/config/swap.ts
+++ src/config/swap.ts
@@ -17,7 +17,6 @@ export const BAKO_FEE_PERCENTAGE = parseInt(FEE);
 export const DEFAULT_SLIPPAGE = 1 * 100; // 1%
 
 export enum SwapButtonTitle {
-  PREVIEW = 'Preview',
   INSUFFICIENT_BALANCE = 'Insufficient Balance',
   INSUFFICIENT_ETH_BALANCE = 'Bridge more ETH to pay for Gas',
   PENDING_TRANSACTION = 'Pending Transaction',
