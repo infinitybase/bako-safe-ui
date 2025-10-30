6c17d74403953b31f47564525be549c8510f4fba
vitor <pereiravitor1218@gmail.com>
2025-10-30 16:52:52 -0300

    refactor(AssetBoxInfo, ContractAddresses): convert components to memoized versions for performance optimization

--- src/modules/transactions/components/TransactionCard/AssetBoxInfo.tsx
+++ src/modules/transactions/components/TransactionCard/AssetBoxInfo.tsx
@@ -10,7 +10,7 @@ import {
   VStack,
 } from 'bako-ui';
 import { bn } from 'fuels';
-import { useMemo } from 'react';
+import { memo, useMemo } from 'react';
 import { FaPlay } from 'react-icons/fa';
 
 import { Address, DoubleArrowIcon, Handle } from '@/components';
@@ -44,300 +44,324 @@ interface AssetBoxInfoProps extends StackProps {
   bridgeImgNet?: string;
 }
 
-const AssetBoxInfo = ({
-  asset,
-  isDeploy,
-  isContract,
-  handle,
-  resolver,
-  rampTransaction,
-  isFiatCurrency = false,
-  bridgeImgNet,
-  ...props
-}: AssetBoxInfoProps) => {
-  const {
-    tokensUSD,
-    screenSizes: { isExtraSmall, isLitteSmall },
-    nftList,
-    assetsMap,
-  } = useWorkspaceContext();
-  const { currentNetwork } = useNetworks();
-  const { getAssetInfo } = useAssetMap(currentNetwork.chainId);
+const AssetBoxInfo = memo(
+  ({
+    asset,
+    isDeploy,
+    isContract,
+    handle,
+    resolver,
+    rampTransaction,
+    isFiatCurrency = false,
+    bridgeImgNet,
+    ...props
+  }: AssetBoxInfoProps) => {
+    const {
+      tokensUSD,
+      screenSizes: { isExtraSmall, isLitteSmall },
+      nftList,
+      assetsMap,
+    } = useWorkspaceContext();
+    const { currentNetwork } = useNetworks();
+    const { getAssetInfo } = useAssetMap(currentNetwork.chainId);
 
-  const { resolveAddressContactHandle } = useAddressNicknameResolver([
-    asset?.to ?? '',
-  ]);
-  const assetAddressInfo = asset?.to
-    ? resolveAddressContactHandle(asset?.to, handle, resolver)
-    : undefined;
+    const { resolveAddressContactHandle } = useAddressNicknameResolver([
+      asset?.to ?? '',
+    ]);
+    const assetAddressInfo = useMemo(
+      () =>
+        asset?.to
+          ? resolveAddressContactHandle(asset?.to, handle, resolver)
+          : undefined,
+      [asset?.to, handle, resolver, resolveAddressContactHandle],
+    );
 
-  const assetInfo = useMemo(() => {
-    return getAssetInfo(asset?.assetId ?? '');
-  }, [asset?.assetId, getAssetInfo]);
+    const assetInfo = useMemo(() => {
+      return getAssetInfo(asset?.assetId ?? '');
+    }, [asset?.assetId, getAssetInfo]);
 
-  const txUSDAmount = useTxAmountToUSD(
-    [
-      asset
-        ? {
-            ...asset,
-            amount: bn(asset?.amount)?.format({
-              units:
-                assetsMap[asset?.assetId]?.units ?? assetsMap.UNKNOWN.units,
-            }),
-          }
-        : {
-            amount: '',
-            assetId: '',
-          },
-    ],
-    tokensUSD?.isLoading,
-    tokensUSD?.data,
-    tokensUSD?.isUnknownToken,
-  );
-  const formattedAmount = bn(asset?.amount)?.format({
-    units: assetsMap?.[asset?.assetId ?? '']?.units ?? assetsMap.UNKNOWN.units,
-  });
+    const txUSDAmount = useTxAmountToUSD(
+      [
+        asset
+          ? {
+              ...asset,
+              amount: bn(asset?.amount)?.format({
+                units:
+                  assetsMap[asset?.assetId]?.units ?? assetsMap.UNKNOWN.units,
+              }),
+            }
+          : {
+              amount: '',
+              assetId: '',
+            },
+      ],
+      tokensUSD?.isLoading,
+      tokensUSD?.data,
+      tokensUSD?.isUnknownToken,
+    );
+    const formattedAmount = useMemo(
+      () =>
+        bn(asset?.amount)?.format({
+          units:
+            assetsMap?.[asset?.assetId ?? '']?.units ?? assetsMap.UNKNOWN.units,
+        }),
+      [asset?.amount, asset?.assetId, assetsMap],
+    );
 
-  const isNFT = useMemo(() => {
-    if (!asset?.assetId) return bn(asset?.amount).eq(bn(1));
+    const isNFT = useMemo(() => {
+      if (!asset?.assetId) return bn(asset?.amount).eq(bn(1));
 
-    const nftAssetIds = new Set(nftList.map((nft) => nft.assetId));
+      const nftAssetIds = new Set(nftList.map((nft) => nft.assetId));
 
-    return nftAssetIds.has(asset.assetId);
-  }, [asset?.assetId, asset?.amount, nftList]);
+      return nftAssetIds.has(asset.assetId);
+    }, [asset?.assetId, asset?.amount, nftList]);
 
-  const image = useMemo(
-    () =>
-      isNFT
-        ? assetInfo?.metadata?.image || assetInfo?.metadata?.['image:png']
-        : assetInfo?.icon,
-    [assetInfo?.icon, assetInfo.metadata, isNFT],
-  );
-  const onRampAmount = useMemo(
-    () =>
-      rampTransaction?.sourceAmount && isFiatCurrency
-        ? moneyFormat(
-            rampTransaction.sourceAmount,
-            rampTransaction.sourceCurrency,
-          )
-        : null,
-    [rampTransaction, isFiatCurrency],
-  );
-  const displayAmount = isNFT ? '1' : onRampAmount || formattedAmount;
+    const image = useMemo(
+      () =>
+        isNFT
+          ? assetInfo?.metadata?.image || assetInfo?.metadata?.['image:png']
+          : assetInfo?.icon,
+      [assetInfo?.icon, assetInfo.metadata, isNFT],
+    );
+    const onRampAmount = useMemo(
+      () =>
+        rampTransaction?.sourceAmount && isFiatCurrency
+          ? moneyFormat(
+              rampTransaction.sourceAmount,
+              rampTransaction.sourceCurrency,
+            )
+          : null,
+      [rampTransaction, isFiatCurrency],
+    );
+    const displayAmount = isNFT ? '1' : onRampAmount || formattedAmount;
 
-  const imgUrl = image ?? (assetsMap?.UNKNOWN?.icon || '');
+    const imgUrl = useMemo(
+      () => image ?? (assetsMap?.UNKNOWN?.icon || ''),
+      [image, assetsMap],
+    );
 
-  const amount = useMemo(
-    () =>
-      isFiatCurrency && rampTransaction?.fiatAmountInUsd
-        ? moneyFormat(rampTransaction?.fiatAmountInUsd?.toString())
-        : txUSDAmount,
-    [isFiatCurrency, rampTransaction?.fiatAmountInUsd, txUSDAmount],
-  );
+    const amount = useMemo(
+      () =>
+        isFiatCurrency && rampTransaction?.fiatAmountInUsd
+          ? moneyFormat(rampTransaction?.fiatAmountInUsd?.toString())
+          : txUSDAmount,
+      [isFiatCurrency, rampTransaction?.fiatAmountInUsd, txUSDAmount],
+    );
 
-  const provider = useMemo(
-    () => (isFiatCurrency ? rampTransaction?.providerTransaction || '-' : null),
-    [isFiatCurrency, rampTransaction?.providerTransaction],
-  );
+    const provider = useMemo(
+      () =>
+        isFiatCurrency ? rampTransaction?.providerTransaction || '-' : null,
+      [isFiatCurrency, rampTransaction?.providerTransaction],
+    );
 
-  const paymentMethodOrProvider = useMemo(
-    () =>
-      isFiatCurrency
-        ? rampTransaction?.paymentMethod
-        : rampTransaction?.providerTransaction,
-    [
-      isFiatCurrency,
-      rampTransaction?.paymentMethod,
-      rampTransaction?.providerTransaction,
-    ],
-  );
+    const paymentMethodOrProvider = useMemo(
+      () =>
+        isFiatCurrency
+          ? rampTransaction?.paymentMethod
+          : rampTransaction?.providerTransaction,
+      [
+        isFiatCurrency,
+        rampTransaction?.paymentMethod,
+        rampTransaction?.providerTransaction,
+      ],
+    );
 
-  return (
-    <Stack
-      p={4}
-      justifyContent={{ base: 'space-between' }}
-      w="full"
-      rounded="lg"
-      bg="bg.muted"
-      mb={1}
-      _last={{
-        mb: 0,
-      }}
-      direction={{
-        base: !rampTransaction ? 'row' : 'column',
-        sm: 'row',
-      }}
-      {...props}
-      border="none"
-    >
-      <HStack
-        justifyContent={{ base: 'space-between', sm: 'flex-start' }}
+    return (
+      <Stack
+        p={4}
+        justifyContent={{ base: 'space-between' }}
         w="full"
+        rounded="lg"
+        bg="bg.muted"
+        mb={1}
+        _last={{
+          mb: 0,
+        }}
+        direction={{
+          base: !rampTransaction ? 'row' : 'column',
+          sm: 'row',
+        }}
+        {...props}
+        border="none"
       >
-        {assetInfo && (
-          <VStack alignItems="start" minW="40px">
-            {bridgeImgNet ? (
-              <Box position="relative" w={6} h={6}>
+        <HStack
+          justifyContent={{ base: 'space-between', sm: 'flex-start' }}
+          w="full"
+        >
+          {assetInfo && (
+            <VStack alignItems="start" minW="40px">
+              {bridgeImgNet ? (
+                <Box position="relative" w={6} h={6}>
+                  <Image
+                    w="full"
+                    h="full"
+                    src={parseURI(imgUrl)}
+                    alt="Asset Icon"
+                    objectFit="cover"
+                    borderRadius="full"
+                  />
+
+                  <Image
+                    w={4}
+                    h={4}
+                    src={parseURI(bridgeImgNet)}
+                    borderRadius="full"
+                    objectFit="cover"
+                    position="absolute"
+                    bottom={-1}
+                    right={-1}
+                    border="2px solid"
+                    borderColor="bg.panel"
+                  />
+                </Box>
+              ) : (
                 <Image
-                  w="full"
-                  h="full"
+                  w={6}
+                  h={6}
                   src={parseURI(imgUrl)}
+                  borderRadius="md"
                   alt="Asset Icon"
                   objectFit="cover"
-                  borderRadius="full"
                 />
+              )}
+            </VStack>
+          )}
 
-                <Image
-                  w={4}
-                  h={4}
-                  src={parseURI(bridgeImgNet)}
-                  borderRadius="full"
-                  objectFit="cover"
-                  position="absolute"
-                  bottom={-1}
-                  right={-1}
-                  border="2px solid"
-                  borderColor="bg.panel"
-                />
-              </Box>
-            ) : (
-              <Image
-                w={6}
-                h={6}
-                src={parseURI(imgUrl)}
-                borderRadius="md"
-                alt="Asset Icon"
-                objectFit="cover"
-              />
-            )}
-          </VStack>
-        )}
-
-        <VStack mt={0.5} flex={1}>
-          <Text
-            textAlign="center"
-            color="textPrimary"
-            fontSize="xs"
-            lineHeight="shorter"
-          >
-            {displayAmount} {assetInfo?.slug}
-          </Text>
-          {FIAT_CURRENCIES_ASSET_IDS.USD !== asset?.assetId && (
+          <VStack mt={0.5} flex={1}>
             <Text
               textAlign="center"
+              color="textPrimary"
               fontSize="xs"
-              color="gray.400"
               lineHeight="shorter"
             >
-              <AmountUSD amount={amount} isNFT={isNFT} />
+              {displayAmount} {assetInfo?.slug}
             </Text>
+            {FIAT_CURRENCIES_ASSET_IDS.USD !== asset?.assetId && (
+              <Text
+                textAlign="center"
+                fontSize="xs"
+                color="gray.400"
+                lineHeight="shorter"
+              >
+                <AmountUSD amount={amount} isNFT={isNFT} />
+              </Text>
+            )}
+          </VStack>
+        </HStack>
+
+        <HStack justifyContent="space-between" w="full">
+          {paymentMethodOrProvider && (
+            <Stack
+              gap={0}
+              minW={{
+                base: 'auto',
+                sm: '200px',
+              }}
+              alignItems={{
+                base: 'start',
+                sm: 'end',
+              }}
+            >
+              <Text color="textPrimary" fontSize="xs">
+                {paymentMethodOrProvider}
+              </Text>
+              <Text color="gray.400" fontSize="xs">
+                {isFiatCurrency ? 'Founds via' : 'Quote By'}
+              </Text>
+            </Stack>
           )}
-        </VStack>
-      </HStack>
 
-      <HStack justifyContent="space-between" w="full">
-        {paymentMethodOrProvider && (
-          <Stack
-            gap={0}
-            minW={{
-              base: 'auto',
-              sm: '200px',
-            }}
-            alignItems={{
-              base: 'start',
-              sm: 'end',
-            }}
-          >
-            <Text color="textPrimary" fontSize="xs">
-              {paymentMethodOrProvider}
-            </Text>
-            <Text color="gray.400" fontSize="xs">
-              {isFiatCurrency ? 'Founds via' : 'Quote By'}
-            </Text>
-          </Stack>
-        )}
+          <Center p={{ base: 1.5, sm: 3 }}>
+            <Icon
+              color="gray.400"
+              boxSize={isDeploy ? '12.8px' : !isContract ? '18px' : '12.8px'}
+              as={
+                isDeploy ? DeployIcon : !isContract ? DoubleArrowIcon : FaPlay
+              }
+            />
+          </Center>
 
-        <Center p={{ base: 1.5, sm: 3 }}>
-          <Icon
-            color="gray.400"
-            boxSize={isDeploy ? '12.8px' : !isContract ? '18px' : '12.8px'}
-            as={isDeploy ? DeployIcon : !isContract ? DoubleArrowIcon : FaPlay}
-          />
-        </Center>
-
-        {(asset?.to || isFiatCurrency) && (
-          <HStack
-            alignItems="center"
-            justifyContent="flex-end"
-            gap={{ base: isLitteSmall ? 0.5 : 1, sm: 2 }}
-            minW={{
-              base: isExtraSmall ? '100px' : isLitteSmall ? '125px' : '135px',
-              sm: '160px',
-              md: '200px',
-            }}
-          >
-            <VStack alignItems="center" gap={1} flex={1}>
-              {provider && (
-                <Stack gap={0} alignItems="end">
-                  <Text
-                    truncate
-                    textOverflow="ellipsis"
-                    maxW={{
-                      base: isExtraSmall
-                        ? '75px'
-                        : isLitteSmall
-                          ? '100px'
-                          : '110px',
-                      sm: '130px',
-                      lg: '130px',
-                    }}
-                    color="textPrimary"
-                    fontSize="xs"
-                  >
-                    {provider}
-                  </Text>
-                  {isFiatCurrency && (
-                    <Text color="gray.500" as="span" fontSize="xs">
-                      Quote By
+          {(asset?.to || isFiatCurrency) && (
+            <HStack
+              alignItems="center"
+              justifyContent="flex-end"
+              gap={{ base: isLitteSmall ? 0.5 : 1, sm: 2 }}
+              minW={{
+                base: isExtraSmall ? '100px' : isLitteSmall ? '125px' : '135px',
+                sm: '160px',
+                md: '200px',
+              }}
+            >
+              <VStack alignItems="center" gap={1} flex={1}>
+                {provider && (
+                  <Stack gap={0} alignItems="end">
+                    <Text
+                      truncate
+                      textOverflow="ellipsis"
+                      maxW={{
+                        base: isExtraSmall
+                          ? '75px'
+                          : isLitteSmall
+                            ? '100px'
+                            : '110px',
+                        sm: '130px',
+                        lg: '130px',
+                      }}
+                      color="textPrimary"
+                      fontSize="xs"
+                    >
+                      {provider}
                     </Text>
+                    {isFiatCurrency && (
+                      <Text color="gray.500" as="span" fontSize="xs">
+                        Quote By
+                      </Text>
+                    )}
+                  </Stack>
+                )}
+
+                <Stack
+                  gap={1}
+                  justifyItems="center"
+                  alignItems="center"
+                  flex={1}
+                >
+                  {assetAddressInfo?.handle && (
+                    <Handle
+                      value={assetAddressInfo.handle}
+                      truncate
+                      alignSelf="center"
+                    />
+                  )}
+                  {asset?.to && (
+                    <Address
+                      value={asset.to}
+                      customValue={AddressUtils.format(asset.to, 5)}
+                      color={
+                        assetAddressInfo?.contact || assetAddressInfo?.handle
+                          ? 'gray.400'
+                          : 'textPrimary'
+                      }
+                    />
                   )}
                 </Stack>
+              </VStack>
+
+              {asset?.to && (
+                <AddressActions
+                  address={asset.to}
+                  handle={assetAddressInfo?.handle}
+                  hasContact={!!assetAddressInfo?.contact}
+                />
               )}
+            </HStack>
+          )}
+        </HStack>
+      </Stack>
+    );
+  },
+);
 
-              <Stack gap={1} justifyItems="center" alignItems="center" flex={1}>
-                {assetAddressInfo?.handle && (
-                  <Handle
-                    value={assetAddressInfo.handle}
-                    truncate
-                    alignSelf="center"
-                  />
-                )}
-                {asset?.to && (
-                  <Address
-                    value={asset.to}
-                    customValue={AddressUtils.format(asset.to, 5)}
-                    color={
-                      assetAddressInfo?.contact || assetAddressInfo?.handle
-                        ? 'gray.400'
-                        : 'textPrimary'
-                    }
-                  />
-                )}
-              </Stack>
-            </VStack>
-
-            {asset?.to && (
-              <AddressActions
-                address={asset.to}
-                handle={assetAddressInfo?.handle}
-                hasContact={!!assetAddressInfo?.contact}
-              />
-            )}
-          </HStack>
-        )}
-      </HStack>
-    </Stack>
-  );
-};
+AssetBoxInfo.displayName = 'AssetBoxInfo';
 
 export { AssetBoxInfo };
--- src/modules/transactions/components/TransactionCard/transfer-details/contract-call/ContractAddresses.tsx
+++ src/modules/transactions/components/TransactionCard/transfer-details/contract-call/ContractAddresses.tsx
@@ -1,4 +1,5 @@
 import { Center, Flex, HStack, Icon, StackProps, Text, VStack } from 'bako-ui';
+import { memo, useMemo } from 'react';
 import { FaPlay } from 'react-icons/fa';
 
 import { Address, Handle } from '@/components';
@@ -15,152 +16,169 @@ interface ContractAddressesProps extends StackProps {
   hasToken?: boolean;
 }
 
-const ContractAddresses = ({
-  transaction,
-  ...props
-}: ContractAddressesProps) => {
-  const {
-    screenSizes: { isExtraSmall, isLitteSmall, isMobile },
-  } = useWorkspaceContext();
-  const { resolveAddressContactHandle } = useAddressNicknameResolver();
+const ContractAddresses = memo(
+  ({ transaction, ...props }: ContractAddressesProps) => {
+    const {
+      screenSizes: { isExtraSmall, isLitteSmall, isMobile },
+    } = useWorkspaceContext();
+    const { resolveAddressContactHandle } = useAddressNicknameResolver();
 
-  const mainOperation = transaction.summary?.operations[0];
+    const mainOperation = useMemo(
+      () => transaction.summary?.operations[0],
+      [transaction.summary],
+    );
 
-  if (!mainOperation) {
-    return null;
-  }
+    const { from, to } = mainOperation || {};
 
-  const { from, to } = mainOperation;
+    const _from = useMemo(
+      () =>
+        from?.address ? resolveAddressContactHandle(from.address) : undefined,
+      [from?.address, resolveAddressContactHandle],
+    );
+    const _to = useMemo(
+      () => (to?.address ? resolveAddressContactHandle(to.address) : undefined),
+      [to?.address, resolveAddressContactHandle],
+    );
 
-  const _from = from?.address
-    ? resolveAddressContactHandle(from.address)
-    : undefined;
-  const _to = to?.address ? resolveAddressContactHandle(to.address) : undefined;
+    if (!mainOperation) {
+      return null;
+    }
 
-  return (
-    <HStack
-      p={4}
-      bg="gray.600"
-      rounded="lg"
-      gap={{ base: 1, sm: 10 }}
-      w="full"
-      justifyContent="space-between"
-      mb={1}
-      _last={{
-        mb: 0,
-      }}
-      {...props}
-    >
-      {from?.address && (
-        <HStack justifyContent="flex-start" gap={{ base: 1, sm: 2 }} flex={1}>
-          <VStack alignItems="start" gap={2}>
-            {_from?.contact && (
-              <Text
-                truncate
-                maxW={{
-                  base: isExtraSmall ? '80px' : isLitteSmall ? '90px' : '125px',
-                  sm: isMobile ? '180px' : '135px',
-                }}
-                fontSize={isExtraSmall ? 'xs' : 'sm'}
-                textAlign="start"
-                color="textPrimary"
-              >
-                {_from.contact}
-              </Text>
-            )}
+    return (
+      <HStack
+        p={4}
+        bg="gray.600"
+        rounded="lg"
+        gap={{ base: 1, sm: 10 }}
+        w="full"
+        justifyContent="space-between"
+        mb={1}
+        _last={{
+          mb: 0,
+        }}
+        {...props}
+      >
+        {from?.address && (
+          <HStack justifyContent="flex-start" gap={{ base: 1, sm: 2 }} flex={1}>
+            <VStack alignItems="start" gap={2}>
+              {_from?.contact && (
+                <Text
+                  truncate
+                  maxW={{
+                    base: isExtraSmall
+                      ? '80px'
+                      : isLitteSmall
+                        ? '90px'
+                        : '125px',
+                    sm: isMobile ? '180px' : '135px',
+                  }}
+                  fontSize={isExtraSmall ? 'xs' : 'sm'}
+                  textAlign="start"
+                  color="textPrimary"
+                >
+                  {_from.contact}
+                </Text>
+              )}
 
-            {!_from?.contact && !_from?.handle && (
-              <Address
-                value={from.address}
-                justifyContent="start"
-                textAlign="start"
-                color="textPrimary"
-              />
-            )}
+              {!_from?.contact && !_from?.handle && (
+                <Address
+                  value={from.address}
+                  justifyContent="start"
+                  textAlign="start"
+                  color="textPrimary"
+                />
+              )}
 
-            {_from?.handle && (
-              <Handle
-                value={_from.handle}
-                truncate
-                textOverflow="ellipsis"
-                maxW={{
-                  base: isExtraSmall ? '55px' : '65px',
-                  sm: isMobile ? '155px' : '110px',
-                }}
-              />
-            )}
-          </VStack>
+              {_from?.handle && (
+                <Handle
+                  value={_from.handle}
+                  truncate
+                  textOverflow="ellipsis"
+                  maxW={{
+                    base: isExtraSmall ? '55px' : '65px',
+                    sm: isMobile ? '155px' : '110px',
+                  }}
+                />
+              )}
+            </VStack>
 
-          <AddressActions
-            address={from?.address}
-            handle={_from?.handle}
-            hasContact={!!_from?.contact}
-          />
-        </HStack>
-      )}
+            <AddressActions
+              address={from?.address}
+              handle={_from?.handle}
+              hasContact={!!_from?.contact}
+            />
+          </HStack>
+        )}
 
-      <Flex alignItems="center" justifyContent="center">
-        <Center
-          borderRadius={5}
-          bgColor="grey.825"
-          borderWidth={1}
-          borderColor="grey.925"
-          boxSize="30px"
-        >
-          <Icon color="grey.250" fontSize="12px" as={FaPlay} />
-        </Center>
-      </Flex>
+        <Flex alignItems="center" justifyContent="center">
+          <Center
+            borderRadius={5}
+            bgColor="grey.825"
+            borderWidth={1}
+            borderColor="grey.925"
+            boxSize="30px"
+          >
+            <Icon color="grey.250" fontSize="12px" as={FaPlay} />
+          </Center>
+        </Flex>
 
-      {to?.address && (
-        <HStack justifyContent="flex-end" gap={{ base: 1, sm: 2 }} flex={1}>
-          <VStack alignItems="end" gap={2}>
-            {_to?.contact && (
-              <Text
-                truncate
-                textOverflow="ellipsis"
-                maxW={{
-                  base: isExtraSmall ? '80px' : isLitteSmall ? '90px' : '125px',
-                  sm: isMobile ? '180px' : '135px',
-                }}
-                fontSize={isExtraSmall ? 'xs' : 'sm'}
-                textAlign="end"
-                color="grey.75"
-              >
-                {_to.contact}
-              </Text>
-            )}
+        {to?.address && (
+          <HStack justifyContent="center" gap={{ base: 1, sm: 2 }} flex={1}>
+            <VStack alignItems="center" gap={2} flex={1}>
+              {_to?.contact && (
+                <Text
+                  truncate
+                  textOverflow="ellipsis"
+                  maxW={{
+                    base: isExtraSmall
+                      ? '80px'
+                      : isLitteSmall
+                        ? '90px'
+                        : '125px',
+                    sm: isMobile ? '180px' : '135px',
+                  }}
+                  fontSize={isExtraSmall ? 'xs' : 'sm'}
+                  textAlign="end"
+                  color="grey.75"
+                >
+                  {_to.contact}
+                </Text>
+              )}
 
-            {!_to?.contact && !_to?.handle && (
-              <Address
-                value={to.address}
-                justifyContent="end"
-                textAlign="end"
-                color={_to?.contact ? 'grey.500' : 'grey.75'}
-              />
-            )}
+              {!_to?.contact && !_to?.handle && (
+                <Address
+                  value={to.address}
+                  justifyContent="end"
+                  textAlign="end"
+                  color={_to?.contact ? 'grey.500' : 'grey.75'}
+                />
+              )}
 
-            {_to?.handle && (
-              <Handle
-                value={_to.handle}
-                truncate
-                textOverflow="ellipsis"
-                maxW={{
-                  base: isExtraSmall ? '55px' : '65px',
-                  sm: isMobile ? '155px' : '110px',
-                }}
-              />
-            )}
-          </VStack>
+              {_to?.handle && (
+                <Handle
+                  value={_to.handle}
+                  truncate
+                  textOverflow="ellipsis"
+                  maxW={{
+                    base: isExtraSmall ? '55px' : '65px',
+                    sm: isMobile ? '155px' : '110px',
+                  }}
+                />
+              )}
+            </VStack>
 
-          <AddressActions
-            address={to?.address}
-            handle={_to?.handle}
-            hasContact={!!_to?.contact}
-          />
-        </HStack>
-      )}
-    </HStack>
-  );
-};
+            <AddressActions
+              address={to?.address}
+              handle={_to?.handle}
+              hasContact={!!_to?.contact}
+            />
+          </HStack>
+        )}
+      </HStack>
+    );
+  },
+);
+
+ContractAddresses.displayName = 'ContractAddresses';
 
 export { ContractAddresses };
