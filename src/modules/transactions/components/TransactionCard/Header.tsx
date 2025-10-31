f69d23c99abe685fe5715f6a3500d9320af71d69
vitor <pereiravitor1218@gmail.com>
2025-10-31 17:40:00 -0300

    fix(Header): remove unnecessary 'as' prop from Root component

--- src/modules/transactions/components/TransactionCard/Header.tsx
+++ src/modules/transactions/components/TransactionCard/Header.tsx
@@ -34,7 +34,6 @@ export const Header = memo(
     return (
       <Root
         onClick={onOpenDialog}
-        as="div"
         w="full"
         _hover={{ bgColor: 'transparent' }}
         px={{ base: 2, sm: 4 }}
