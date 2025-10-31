c743cc1bc42d8ed75019f18d668e75e3df9f1958
vitor <pereiravitor1218@gmail.com>
2025-10-31 11:55:44 -0300

    fix(NotificationsEmptyState): correct border and text colors for improved visibility

--- src/modules/notifications/components/emptyState/index.tsx
+++ src/modules/notifications/components/emptyState/index.tsx
@@ -4,7 +4,7 @@ import { EmptyBoxOutline } from '@/components';
 
 const NotificationsEmptyState = () => {
   return (
-    <Box h={'100%'} borderRadius={10} borderWidth={1} borderColor="grey.800">
+    <Box h={'100%'} borderRadius={10} borderWidth={1} borderColor="gray.400">
       <Card.Root
         w="full"
         h={'100%'}
@@ -21,7 +21,7 @@ const NotificationsEmptyState = () => {
 
         <Box mb={4}>
           <Heading
-            color="grey.75"
+            color="textPrimary"
             fontSize="lg"
             textAlign="center"
             fontWeight={600}
@@ -32,7 +32,7 @@ const NotificationsEmptyState = () => {
 
         <Box>
           <Text
-            color="grey.250"
+            color="textSecondary"
             fontSize="xs"
             textAlign="center"
             fontWeight={200}
