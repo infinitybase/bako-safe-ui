a193bb97cfa7e6da5509e0697d83c07072df5580
vitor <pereiravitor1218@gmail.com>
2025-10-31 11:55:57 -0300

    refactor(NotificationsDrawer, NotificationCard): update props to use DrawerRootProps and CardRootProps; enhance styling for better UI consistency

--- src/modules/notifications/components/drawer/index.tsx
+++ src/modules/notifications/components/drawer/index.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Center,
   Drawer,
+  DrawerRootProps,
   Heading,
   HStack,
   Portal,
@@ -15,7 +16,7 @@ import { useAppNotifications } from '../../hooks';
 import { NotificationsEmptyState } from '../emptyState';
 import { NotificationCard } from '../notificationCard';
 
-interface NotificationsDrawerProps extends Omit<Drawer.RootProps, 'children'> {}
+interface NotificationsDrawerProps extends Omit<DrawerRootProps, 'children'> {}
 
 const NotificationsDrawer = ({ ...props }: NotificationsDrawerProps) => {
   const {
@@ -37,14 +38,13 @@ const NotificationsDrawer = ({ ...props }: NotificationsDrawerProps) => {
     <Drawer.Root
       {...props}
       size="sm"
-      // variant="solid-dark"
       placement="end"
       onOpenChange={drawer.onClose}
     >
       <Portal>
         <Drawer.Backdrop />
         <Drawer.Positioner>
-          <Drawer.Content p={10}>
+          <Drawer.Content p={6}>
             <Drawer.Header mb={6}>
               <VStack alignItems="flex-start" gap={4}>
                 <HStack
@@ -55,49 +55,50 @@ const NotificationsDrawer = ({ ...props }: NotificationsDrawerProps) => {
                 >
                   <HStack gap={2} alignItems="center">
                     <Heading
-                      fontSize="lg"
+                      fontSize="xl"
                       fontWeight="semibold"
-                      color="grey.50"
+                      color="textPrimary"
                     >
                       Notifications
                     </Heading>
                     {unreadCounter > 0 && (
                       <Center
-                        minW={4}
-                        minH={4}
-                        maxW={9}
-                        maxH={9}
-                        bg="error.600"
-                        borderRadius={10}
+                        bg="red"
+                        minW="16px"
+                        h="16px"
+                        borderRadius="full"
                         px={unreadCounter > 99 ? '0.5' : '0'}
                       >
-                        <Text fontSize="xs">
+                        <Text
+                          fontSize="2xs"
+                          color="gray.50"
+                          fontWeight="bold"
+                          lineHeight="shorter"
+                        >
                           {unreadCounter > 99 ? '+99' : unreadCounter}
                         </Text>
                       </Center>
                     )}
                   </HStack>
-                  <LineCloseIcon
-                    w="24px"
-                    aria-label="Close window"
-                    cursor="pointer"
-                    onClick={drawer.onClose}
-                  />
+
+                  <Drawer.CloseTrigger position="static">
+                    <LineCloseIcon
+                      w="24px"
+                      aria-label="Close window"
+                      cursor="pointer"
+                    />
+                  </Drawer.CloseTrigger>
                 </HStack>
-                <Text
-                  fontSize="xs"
-                  color="grey.450"
-                  fontWeight={400}
-                  lineHeight={1.21}
-                >
-                  {`Stay informed about all the activities happening in the vaults that you are part of.`}
+                <Text fontSize="md" color="textSecondary" lineHeight={1.21}>
+                  Stay informed about all the activities happening in the vaults
+                  that you are part of
                 </Text>
               </VStack>
             </Drawer.Header>
             {hasNotifications ? (
               <Drawer.Body
-                borderTop={!hasNotifications ? 0 : '1px'}
-                borderTopColor="dark.100"
+                borderTop="1px solid"
+                borderTopColor="gray.400"
                 py={isSuccess && !hasNotifications ? 0 : 8}
                 css={{
                   '::-webkit-scrollbar': { width: 0 },
