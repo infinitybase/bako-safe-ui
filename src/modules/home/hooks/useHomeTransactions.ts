fdb88bde368de69a1f35d5dffa090c35a7994609
vitor <pereiravitor1218@gmail.com>
2025-10-31 16:54:19 -0300

    feat(useHomeTransactions): add filtering functionality for transaction status

--- src/modules/home/hooks/useHomeTransactions.ts
+++ src/modules/home/hooks/useHomeTransactions.ts
@@ -1,4 +1,8 @@
+import { useCallback, useState } from 'react';
+
 import { useGroupTransactionsByDay } from '@/modules/core/hooks/useGroupTransactionsByDay';
+import { StatusFilter } from '@/modules/transactions';
+import { useFilterTxType } from '@/modules/transactions/hooks/filter';
 
 import { useHomeTransactionsRequest } from './useHomeTransationsRequest';
 
@@ -7,6 +11,24 @@ export type IUseHomeTransactionsReturn = ReturnType<typeof useHomeTransactions>;
 const useHomeTransactions = (workspaceId: string) => {
   const { data, isFetching, isLoading, refetch } =
     useHomeTransactionsRequest(workspaceId);
+  const [filter, setFilter] = useState<StatusFilter>(StatusFilter.ALL);
+
+  const handleResetStatusFilter = useCallback(() => {
+    if (filter !== StatusFilter.ALL) setFilter(StatusFilter.ALL);
+  }, [filter]);
+
+  const {
+    txFilterType,
+    handleIncomingAction,
+    handleOutgoingAction,
+    setTxFilterType,
+    handleAllAction,
+  } = useFilterTxType(handleResetStatusFilter);
+
+  const handlePendingStatusChange = () => {
+    setTxFilterType(undefined);
+    setFilter(StatusFilter.PENDING);
+  };
 
   return {
     transactions: useGroupTransactionsByDay(data?.data),
@@ -14,6 +36,16 @@ const useHomeTransactions = (workspaceId: string) => {
       isFetching,
       isLoading,
       refetch,
+    },
+    filter: {
+      filter,
+      setFilter,
+      txFilterType,
+      handleIncomingAction,
+      handleOutgoingAction,
+      setTxFilterType,
+      handleAllAction,
+      handlePendingStatusChange,
     },
   };
 };
