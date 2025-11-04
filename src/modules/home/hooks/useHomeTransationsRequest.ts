ae4d50f64aacf635bbe671cae5ecc93f32b2ebf8
vitor <pereiravitor1218@gmail.com>
2025-11-04 15:38:53 -0300

    feat(HomeTransactions): implement infinite scrolling for transactions and enhance query parameters

--- src/modules/home/hooks/useHomeTransationsRequest.ts
+++ src/modules/home/hooks/useHomeTransationsRequest.ts
@@ -1,16 +1,42 @@
-import { useQuery } from '@tanstack/react-query';
-
-import { HomeQueryKey } from '@/modules/core/models';
-
-import { HomeService } from '../services';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { TransactionType } from 'bakosafe';
 import { useLocation } from 'react-router-dom';
 
-const useHomeTransactionsRequest = (workspaceId: string) => {
+import { HomeQueryKey } from '@/modules/core/models';
+import { ITransactionStatusFilter } from '@/modules/transactions/services';
+
+import { HomeService } from '../services';
+
+const useHomeTransactionsRequest = ({
+  workspaceId,
+  status,
+  type,
+}: {
+  workspaceId: string;
+  type?: TransactionType;
+  status?: ITransactionStatusFilter;
+  offSetDb?: string | number;
+}) => {
   const location = useLocation();
 
-  return useQuery({
-    queryKey: HomeQueryKey.HOME_WORKSPACE(workspaceId),
-    queryFn: () => HomeService.homeTransactions(),
+  return useInfiniteQuery({
+    queryKey: HomeQueryKey.HOME_WORKSPACE(workspaceId, status, type),
+    initialPageParam: { offsetDb: 0, offsetFuel: 0 },
+    queryFn: ({ pageParam: { offsetDb, offsetFuel } }) =>
+      HomeService.homeTransactions({
+        type,
+        status,
+        perPage: 5,
+        offsetDb,
+        offsetFuel,
+      }),
+    getNextPageParam: (lastPage) => {
+      if (lastPage?.data?.length === 0) {
+        return undefined;
+      }
+
+      return { offsetDb: lastPage.offsetDb, offsetFuel: lastPage.offsetFuel };
+    },
     refetchOnWindowFocus: true,
     enabled: !!workspaceId && location.pathname != '/',
     refetchOnMount: false,
