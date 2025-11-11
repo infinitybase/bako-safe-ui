7dc886a5ccaea99034012cd46f32b22feed2242d
pedroanastacio <pedro.anastacio013@gmail.com>
2025-11-11 17:36:44 -0300

    fix(dapp): adjusts login screens with evm wallets and social login with design system components

--- src/modules/dapp/components/signIn/container.tsx
+++ src/modules/dapp/components/signIn/container.tsx
@@ -1,4 +1,4 @@
-import { Button, Image, Stack, Text, VStack } from '@chakra-ui/react';
+import { Button, Image, Stack, Text, VStack } from 'bako-ui';
 
 import bakoLogo from '@/assets/bako-logo.svg';
 import { EConnectors } from '@/modules/core/hooks/fuel/useListConnectors';
@@ -13,32 +13,19 @@ export const DappSignInContainer = (props: DappSignInContainerProps) => {
   const { message, disableConnect, onConnect } = props;
 
   return (
-    <VStack
-      w="100%"
-      minH="$100vh"
-      padding={4}
-      overflow="hidden"
-      __css={{
-        '&::-webkit-scrollbar': {
-          display: 'none',
-        },
-        '&::-webkit-scrollbar-thumb': {
-          display: 'none',
-        },
-      }}
-    >
+    <VStack w="100%" minH="100vh" padding={4} overflow="hidden">
       <Stack flex={1} justifyContent="flex-end">
         <Image width={{ base: 250, sm: 300 }} src={bakoLogo} alt="Bako Logo" />
       </Stack>
 
-      <VStack flex={5} justifyContent="center" alignItems="center" spacing={4}>
+      <VStack flex={5} justifyContent="center" alignItems="center" gap={4}>
         <Text textAlign="center" color="gray.100" fontSize="small">
           {message}
         </Text>
         <Button
           w="full"
           maxW={{ base: 190, sm: 230 }}
-          variant="secondaryV2"
+          variant="subtle"
           disabled={disableConnect}
           onClick={() => onConnect()}
         >
