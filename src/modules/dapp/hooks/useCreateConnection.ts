46b3e23fca5cf9b9ecdc79c327180a7ada3ac9de
pedroanastacio <pedro.anastacio013@gmail.com>
2025-11-18 15:01:20 -0300

    fix(dapp): connector should close the window after a successful connection

--- src/modules/dapp/hooks/useCreateConnection.ts
+++ src/modules/dapp/hooks/useCreateConnection.ts
@@ -6,9 +6,6 @@ import { IDAppCreatePayload } from '../services/methods';
 const useCreateConnections = () => {
   return useMutation({
     mutationFn: (params: IDAppCreatePayload) => DAppService.create(params),
-    onSuccess: () => {
-      window.close();
-    },
   });
 };
 
