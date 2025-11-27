c591787319631fc9f98205b83aaa446e39f30a90
pedroanastacio <pedro.anastacio013@gmail.com>
2025-11-27 09:32:13 -0300

    refactor(dapp): adds vault card to common components

--- src/modules/dapp/components/transaction/operation-card.tsx
+++ src/modules/dapp/components/transaction/operation-card.tsx
@@ -1,17 +1,6 @@
-import {
-  Avatar,
-  Box,
-  Flex,
-  HStack,
-  IconButton,
-  Text,
-  Tooltip,
-  useClipboard,
-  VStack,
-} from 'bako-ui';
+import { Box, Flex, Text, useClipboard, VStack } from 'bako-ui';
 import { bn } from 'fuels';
-import { useCallback, useMemo } from 'react';
-import { PiCopyThin } from 'react-icons/pi';
+import { useMemo } from 'react';
 
 import { useWorkspaceContext } from '@/modules';
 import { AddressUtils } from '@/modules/core';
@@ -104,83 +93,6 @@ const AssetDisplay = ({ assets }: AssetDisplayProps) => {
   );
 };
 
-interface AddressDisplayProps {
-  address: string;
-  name?: string;
-  isCurrentAccount: boolean;
-  vaultAddress?: string;
-  onCopy: () => void;
-  hasCopied: boolean;
-}
-
-const AddressDisplay = ({
-  address,
-  name,
-  isCurrentAccount,
-  vaultAddress,
-  onCopy,
-  hasCopied,
-}: AddressDisplayProps) => {
-  const handleCopy = useCallback(() => {
-    if (isCurrentAccount && vaultAddress) {
-      onCopy();
-    } else {
-      navigator.clipboard.writeText(address);
-    }
-  }, [isCurrentAccount, vaultAddress, onCopy, address]);
-  return (
-    <Flex w="full" alignItems="center" bg="gray.700" borderRadius={8} p={4}>
-      <HStack gap={3} align="center">
-        <Avatar
-          shape="rounded"
-          color="gray.100"
-          bgColor="gray.500"
-          boxSize="36px"
-          size="xs"
-          name={name ?? '?'}
-        />
-        <VStack gap={2} align="flex-start">
-          {name && (
-            <Text
-              fontWeight={500}
-              color="gray.100"
-              fontSize="xs"
-              lineHeight="12px"
-            >
-              {name}
-            </Text>
-          )}
-          <Flex align="center" gap={1}>
-            <Text
-              fontWeight={name ? 400 : 500}
-              color={name ? 'gray.400' : 'gray.100'}
-              fontSize="xs"
-              lineHeight="12px"
-            >
-              {AddressUtils.format(address, 15)}
-            </Text>
-            <Tooltip content={hasCopied ? 'Copied!' : 'Copy'} closeOnClick>
-              <IconButton
-                aria-label="Copy address"
-                size="xs"
-                boxSize="12px"
-                onClick={handleCopy}
-                color="gray.400"
-                variant="ghost"
-                _hover={{ background: 'transparent' }}
-                _active={{ background: 'transparent' }}
-                _focus={{ boxShadow: 'none' }}
-              >
-                <PiCopyThin />
-              </IconButton>
-            </Tooltip>
-          </Flex>
-        </VStack>
-      </HStack>
-    </Flex>
-  );
-};
-
 interface DappTransactionOperationCardProps {
   operation: SimplifiedOperation;
   vault?: UseTransactionSocket['vault'];
@@ -201,7 +113,7 @@ export const DappTransactionOperationCard = (
 
   return (
     <VStack gap={1} w="full">
-      <AddressDisplay
+      <DappCommon.VaultCard
         address={operation.from.address}
         name={
           operation.isFromCurrentAccount ? vault?.name || 'Vault' : undefined
@@ -218,7 +130,7 @@ export const DappTransactionOperationCard = (
         label={isContract ? 'Calling contract' : 'Sending funds'}
       />
 
-      <AddressDisplay
+      <DappCommon.VaultCard
         address={operation.to.address}
         name={operation.isToCurrentAccount ? vault?.name || 'Vault' : undefined}
         isCurrentAccount={operation.isToCurrentAccount ?? false}
@@ -233,7 +145,7 @@ export const DappTransactionOperationCard = (
 
           <DappCommon.OperationArrow label="Sends funds" />
 
-          <AddressDisplay
+          <DappCommon.VaultCard
             address={operation.from.address}
             name={
               operation.isFromCurrentAccount
