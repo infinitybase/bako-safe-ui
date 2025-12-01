78caac0abbd9c490c6cf3b3492e1ddccbe46bcef
vitor <pereiravitor1218@gmail.com>
2025-12-01 16:14:53 -0300

    fix(asset): reorganize imports and enhance asset selection component structure

--- src/modules/transactions/components/dialog/create/recipient/form/asset.tsx
+++ src/modules/transactions/components/dialog/create/recipient/form/asset.tsx
@@ -3,8 +3,8 @@ import { bn } from 'fuels';
 import { memo, useCallback, useMemo } from 'react';
 import { FieldError, useFormContext } from 'react-hook-form';
 
-import { AssetSelectOption } from '@/components';
-import { AssetSelect, UseVaultDetailsReturn } from '@/modules';
+import { AssetSelect, AssetSelectOption } from '@/components';
+import { UseVaultDetailsReturn } from '@/modules';
 
 interface RecipientFormAssetProps {
   assets: UseVaultDetailsReturn['assets'];
@@ -60,42 +60,41 @@ const RecipientFormAsset = ({
       width="100%"
       data-testid="transaction_asset"
     >
-      <AssetSelect
-        isInvalid={isInvalid}
-        options={assetsOptions}
-        name={`transaction.${index}.asset`}
-        value={value}
-        onChange={(e) => {
-          handleUpdateAmount(isNFTAsset(e), e);
-        }}
-        helperText={
-          <Field.HelperText color={error?.message ? 'error.500' : 'grey.425'}>
-            {!isNFT && (
-              <Text display="flex" alignItems="center" mt={1}>
-                {!value ? (
-                  'Select an asset to see the balance'
-                ) : parseFloat(balanceAvailable) > 0 ? (
-                  isFeeCalcLoading ? (
-                    <>
-                      Balance (available):{' '}
-                      <Loader
-                        css={{ '--spinner-track-color': 'dark.100' }}
-                        size="xs"
-                        color="grey.425"
-                        ml={1}
-                      />
-                    </>
-                  ) : (
-                    <>
-                      Balance (available): {slug} {balanceAvailable}
-                    </>
-                  )
-                ) : null}
-              </Text>
-            )}
-          </Field.HelperText>
-        }
-      />
+      <Field.Root invalid={!!error?.message}>
+        <AssetSelect
+          isInvalid={isInvalid}
+          options={assetsOptions}
+          placeholder="Select Token"
+          name={`transaction.${index}.asset`}
+          value={value}
+          onChange={(e) => {
+            handleUpdateAmount(isNFTAsset(e), e);
+          }}
+        />
+        <Field.HelperText color={error?.message ? 'error.500' : 'grey.425'}>
+          {!isNFT && (
+            <Text display="flex" alignItems="center" mt={1}>
+              {value && parseFloat(balanceAvailable) > 0 ? (
+                isFeeCalcLoading ? (
+                  <>
+                    Balance (available):{' '}
+                    <Loader
+                      css={{ '--spinner-track-color': 'dark.100' }}
+                      size="xs"
+                      color="grey.425"
+                      ml={1}
+                    />
+                  </>
+                ) : (
+                  <>
+                    Balance (available): {slug} {balanceAvailable}
+                  </>
+                )
+              ) : null}
+            </Text>
+          )}
+        </Field.HelperText>
+      </Field.Root>
     </HStack>
   );
 };
