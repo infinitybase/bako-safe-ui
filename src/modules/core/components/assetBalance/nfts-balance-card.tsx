3ed8f4478f2f49f91baae46eec3bbb452fd07156
pedroanastacio <pedro.anastacio013@gmail.com>
2025-12-04 10:55:17 -0300

    fix(nft): adjusts text color

--- src/modules/core/components/assetBalance/nfts-balance-card.tsx
+++ src/modules/core/components/assetBalance/nfts-balance-card.tsx
@@ -53,7 +53,7 @@ const NftBalanceCard = memo(function NftBalanceCard({ nft }: { nft: NFT }) {
         <Card.Body>
           <Text
             fontSize="sm"
-            color="gray.50"
+            color="gray.200"
             truncate
             textAlign="center"
             letterSpacing="wider"
