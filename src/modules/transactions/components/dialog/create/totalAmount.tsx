37671d1a249208b27bd479acf44d8459abf4119a
Gabriel Tozatti <gabrieltozatti2021@gmail.com>
2026-02-05 16:55:36 -0300

    fix: create interface for totalAmount typing

--- src/modules/transactions/components/dialog/create/totalAmount.tsx
+++ src/modules/transactions/components/dialog/create/totalAmount.tsx
@@ -1,26 +1,28 @@
 import { Flex, HStack, Text } from 'bako-ui';
 import { memo } from 'react';
 
-export const TotalAmount = memo(
-  ({ totalAmount }: { totalAmount: string | undefined }) => {
-    return (
-      <Flex
-        wrap="wrap"
-        justifyContent="space-between"
-        alignItems="center"
-        w="full"
-      >
-        <HStack align="center" gap={1}>
-          <Text visibility={!totalAmount ? 'hidden' : 'visible'} fontSize="xs">
-            Total Amount
-          </Text>
-        </HStack>
-        <HStack align="center" gap={1}>
-          <Text fontSize="xs">{totalAmount && `~${totalAmount}`}</Text>
-        </HStack>
-      </Flex>
-    );
-  },
-);
+interface TotalAmountProps {
+  totalAmount?: string;
+}
+
+export const TotalAmount = memo(({ totalAmount }: TotalAmountProps) => {
+  return (
+    <Flex
+      wrap="wrap"
+      justifyContent="space-between"
+      alignItems="center"
+      w="full"
+    >
+      <HStack align="center" gap={1}>
+        <Text visibility={!totalAmount ? 'hidden' : 'visible'} fontSize="xs">
+          Total Amount
+        </Text>
+      </HStack>
+      <HStack align="center" gap={1}>
+        <Text fontSize="xs">{totalAmount && `~${totalAmount}`}</Text>
+      </HStack>
+    </Flex>
+  );
+});
 
 TotalAmount.displayName = 'TotalAmount';
