9e0c50acc8fe02e91d1e6ee6ec540d6ce2f24550
pedroanastacio <pedro.anastacio013@gmail.com>
2026-03-02 10:24:10 -0300

    refactor(deposit): add semantic values

--- src/modules/transactions/components/TransactionCard/deposit-details/DetailItemAddress.tsx
+++ src/modules/transactions/components/TransactionCard/deposit-details/DetailItemAddress.tsx
@@ -36,7 +36,7 @@ export const DetailItemAddress = memo(
             <Text
               truncate
               textOverflow="ellipsis"
-              maxW={{ base: 100, sm: 180 }}
+              maxW={{ base: '100px', sm: '180px' }}
               fontSize="xs"
               color="textPrimary"
             >
